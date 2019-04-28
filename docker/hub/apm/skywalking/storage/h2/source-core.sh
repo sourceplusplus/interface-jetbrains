5d60317e254a494bc6388535f8dd2f08f0f0ae48
Brandon Fergerson <brandon.fergerson@codebrig.com>
2019-04-27 19:36:06 -0600

    can hit outside docker

--- docker/hub/apm/skywalking/storage/h2/source-core.sh
+++ docker/hub/apm/skywalking/storage/h2/source-core.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-$SKYWALKING_HOME/bin/oapService.sh && $SOURCE_HOME/bin/source-core
+$SKYWALKING_HOME/bin/oapService.sh
+
+$SOURCE_HOME/bin/source-core
 tail -f -n +1 $SOURCE_HOME/logs/source-core.log
