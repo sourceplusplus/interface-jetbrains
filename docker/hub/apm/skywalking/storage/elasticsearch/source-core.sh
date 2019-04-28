2537a57983ee9ab87b0b080f68c52252d214bda5
Brandon Fergerson <brandon.fergerson@codebrig.com>
2019-04-27 21:10:58 -0600

    make skywalking use elasticsearch

--- docker/hub/apm/skywalking/storage/elasticsearch/source-core.sh
+++ docker/hub/apm/skywalking/storage/elasticsearch/source-core.sh
@@ -4,6 +4,7 @@ nohup /usr/share/elasticsearch/bin/elasticsearch &
 sleep 15
 
 $SKYWALKING_HOME/bin/oapService.sh
+sleep 30
 
 $SOURCE_HOME/bin/source-core
 tail -f -n +1 $SOURCE_HOME/logs/source-core.log
