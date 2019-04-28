42c5e367c5ac8c34deb5d65ae61f40e83c51c50e
Brandon Fergerson <brandon.fergerson@codebrig.com>
2019-04-27 20:54:46 -0600

    basic elasticsearch impl

--- docker/hub/apm/skywalking/storage/elasticsearch/source-core.sh
+++ docker/hub/apm/skywalking/storage/elasticsearch/source-core.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+nohup /usr/share/elasticsearch/bin/elasticsearch &
+sleep 15
+
+$SKYWALKING_HOME/bin/oapService.sh
+
 $SOURCE_HOME/bin/source-core
 tail -f -n +1 $SOURCE_HOME/logs/source-core.log
