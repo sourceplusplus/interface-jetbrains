18376019c98cb37e04828c8e641790cac18ada1a
Brandon Fergerson <brandon.fergerson@codebrig.com>
2019-06-16 17:14:28 -0600

    comment

--- api/src/main/java/com/sourceplusplus/api/model/integration/config/IntegrationConfigTypeResolver.java
+++ api/src/main/java/com/sourceplusplus/api/model/integration/config/IntegrationConfigTypeResolver.java
@@ -5,6 +5,13 @@ import com.fasterxml.jackson.databind.DatabindContext;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.jsontype.impl.TypeIdResolverBase;
 
+/**
+ * todo: description
+ *
+ * @author <a href="mailto:brandon@srcpl.us">Brandon Fergerson</a>
+ * @version 0.2.0
+ * @since 0.2.0
+ */
 public class IntegrationConfigTypeResolver extends TypeIdResolverBase {
 
     private JavaType superType;
