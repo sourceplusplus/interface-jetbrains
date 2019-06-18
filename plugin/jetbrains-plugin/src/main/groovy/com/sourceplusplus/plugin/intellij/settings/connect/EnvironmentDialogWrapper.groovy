fef2140f9b744fbdf80e61ccea26e8e4bf6adf4f
Brandon Fergerson <fergerson92@gmail.com>
2019-06-17 22:26:44 -0600

    Update EnvironmentDialogWrapper.groovy

--- plugin/jetbrains-plugin/src/main/groovy/com/sourceplusplus/plugin/intellij/settings/connect/EnvironmentDialogWrapper.groovy
+++ plugin/jetbrains-plugin/src/main/groovy/com/sourceplusplus/plugin/intellij/settings/connect/EnvironmentDialogWrapper.groovy
@@ -11,7 +11,7 @@ import javax.swing.*
  * todo: description
  *
  * @version 0.2.0
- * @since 0.1.0
+ * @since 0.2.0
  * @author <a href="mailto:brandon@srcpl.us">Brandon Fergerson</a>
  */
 class EnvironmentDialogWrapper extends DialogWrapper {
