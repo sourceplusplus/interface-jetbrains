ee7a38560f4687b14e965d1d6cd8eef595ece0c5
Brandon Fergerson <brandon.fergerson@codebrig.com>
2019-06-28 10:38:21 -0600

    fixes #89

--- agent/jvm-agent/src/main/java/com/sourceplusplus/agent/SourceAgent.java
+++ agent/jvm-agent/src/main/java/com/sourceplusplus/agent/SourceAgent.java
@@ -159,7 +159,8 @@ public class SourceAgent {
                         application = coreClient.findApplicationByName(SourceAgentConfig.current.appName);
                     }
                     if (application != null) {
-                        Logger.warn(String.format("Found application. App Name: %s - App UUID: %s",
+                        SourceAgentConfig.current.appUuid = application.appUuid();
+                        Logger.info(String.format("Found application. App Name: %s - App UUID: %s",
                                 application.appName(), application.appUuid()));
 
                         if (application.agentConfig() != null) {
