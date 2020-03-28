86587b58780dfd95d61750f7d19e191899aa26cf
Brandon Fergerson <fergerson92@gmail.com>
2020-03-28 12:23:40 -0400

    add missing javadoc

--- plugin/jetbrains-plugin/src/main/groovy/com/sourceplusplus/plugin/source/model/SourceMethodAnnotation.groovy
+++ plugin/jetbrains-plugin/src/main/groovy/com/sourceplusplus/plugin/source/model/SourceMethodAnnotation.groovy
@@ -3,6 +3,13 @@ package com.sourceplusplus.plugin.source.model
 import groovy.transform.Immutable
 import groovy.transform.TupleConstructor
 
+/**
+ * todo: description
+ *
+ * @version 0.2.3
+ * @since 0.1.0
+ * @author <a href="mailto:brandon@srcpl.us">Brandon Fergerson</a>
+ */
 @Immutable
 @TupleConstructor
 class SourceMethodAnnotation {
