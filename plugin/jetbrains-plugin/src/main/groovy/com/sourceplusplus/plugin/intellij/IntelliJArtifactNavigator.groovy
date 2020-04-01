9da15201df1bef3cf107866a96e809c9f07c11b0
Brandon Fergerson <fergerson92@gmail.com>
2020-04-01 17:15:40 -0400

    version corrections

--- plugin/jetbrains-plugin/src/main/groovy/com/sourceplusplus/plugin/intellij/IntelliJArtifactNavigator.groovy
+++ plugin/jetbrains-plugin/src/main/groovy/com/sourceplusplus/plugin/intellij/IntelliJArtifactNavigator.groovy
@@ -15,7 +15,7 @@ import plus.sourceplus.marker.source.mark.gutter.GutterMark
 /**
  * todo: description
  *
- * @version 0.2.3
+ * @version 0.2.4
  * @since 0.1.0
  * @author <a href="mailto:brandon@srcpl.us">Brandon Fergerson</a>
  */
--- plugin/jetbrains-plugin/src/main/groovy/com/sourceplusplus/plugin/source/model/SourceMethodAnnotation.groovy
+++ plugin/jetbrains-plugin/src/main/groovy/com/sourceplusplus/plugin/source/model/SourceMethodAnnotation.groovy
@@ -6,7 +6,7 @@ import groovy.transform.TupleConstructor
 /**
  * todo: description
  *
- * @version 0.2.3
+ * @version 0.2.4
  * @since 0.1.0
  * @author <a href="mailto:brandon@srcpl.us">Brandon Fergerson</a>
  */
