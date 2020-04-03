1866f8c297c67b7b6f1aeb1e772e7829b246ba5d
Brandon Fergerson <fergerson92@gmail.com>
2020-04-03 09:15:19 -0400

    consistency

--- api/src/main/java/com/sourceplusplus/api/bridge/SourceBridgeClient.java
+++ api/src/main/java/com/sourceplusplus/api/bridge/SourceBridgeClient.java
@@ -11,7 +11,7 @@ import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 
 /**
- * todo: this
+ * todo: description
  *
  * @author <a href="mailto:brandon@srcpl.us">Brandon Fergerson</a>
  * @version 0.2.4
