2e827b7a5b6d7fee52b36c36ccf8a6dbc88453a1
Brandon Fergerson <bfergerson@apache.org>
2020-06-19 17:38:18 +0400

    test skywalking using elasticsearch

--- core/src/test/groovy/test/integration/trace/TraceAPITest.groovy
+++ core/src/test/groovy/test/integration/trace/TraceAPITest.groovy
@@ -149,7 +149,7 @@ class TraceAPITest extends SourceCoreAPITest {
                         test.fail(it.cause())
                     }
 
-                    for (int i = 0; i < 40; i++) {
+                    for (int i = 0; i < 80; i++) { //todo: using h2 can put this down to 40
                         TraceTest.threeStaticMethodCallDepth()
                     }
 
