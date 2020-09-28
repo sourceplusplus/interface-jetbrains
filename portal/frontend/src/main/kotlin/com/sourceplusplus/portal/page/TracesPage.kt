8f7e463ad26f41bd0d0e065224ae8f79ffb23875
chess-equality <chessequality@gmail.com>
2020-09-28 14:00:08 +0800

    [#251] Initially converted traces.js

--- portal/frontend/src/main/kotlin/com/sourceplusplus/portal/page/TracesPage.kt
+++ portal/frontend/src/main/kotlin/com/sourceplusplus/portal/page/TracesPage.kt
@@ -1,6 +1,15 @@
 package com.sourceplusplus.portal.page
 
+import com.sourceplusplus.portal.extensions.eb
 import com.sourceplusplus.portal.template.*
+import com.sourceplusplus.protocol.ProtocolAddress.Global.Companion.ClickedDisplaySpanInfo
+import com.sourceplusplus.protocol.ProtocolAddress.Global.Companion.ClickedDisplayTraceStack
+import com.sourceplusplus.protocol.ProtocolAddress.Global.Companion.ClickedDisplayTraces
+import com.sourceplusplus.protocol.ProtocolAddress.Global.Companion.TracesTabOpened
+import com.sourceplusplus.protocol.ProtocolAddress.Portal.Companion.DisplayInnerTraceStack
+import com.sourceplusplus.protocol.ProtocolAddress.Portal.Companion.DisplaySpanInfo
+import com.sourceplusplus.protocol.ProtocolAddress.Portal.Companion.DisplayTraceStack
+import com.sourceplusplus.protocol.ProtocolAddress.Portal.Companion.DisplayTraces
 import com.sourceplusplus.protocol.artifact.trace.TraceOrderType.*
 import com.sourceplusplus.protocol.artifact.trace.TraceSpanInfoType.END_TIME
 import com.sourceplusplus.protocol.artifact.trace.TraceSpanInfoType.START_TIME
@@ -11,6 +20,7 @@ import com.sourceplusplus.protocol.portal.PageType.*
 import kotlinx.browser.document
 import kotlinx.html.dom.append
 import org.w3c.dom.Element
+import kotlin.js.json
 
 /**
  * todo: description.
@@ -19,6 +29,28 @@ import org.w3c.dom.Element
  * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
  */
 class TracesPage {
+    private val portalUuid = "null"
+    init {
+        console.log("Traces tab started");
+        eb.onopen = {
+            js("portalConnected();")
+            eb.registerHandler(DisplayTraces(portalUuid)) { error: String, message: Any ->
+                js("displayTraces(message.body);")
+            }
+            eb.registerHandler(DisplayInnerTraceStack(portalUuid)) { error: String, message: Any ->
+                js("displayInnerTraces(message.body);")
+            }
+            eb.registerHandler(DisplayTraceStack(portalUuid)) { error: String, message: Any ->
+                js("displayTraceStack(message.body);")
+            }
+            eb.registerHandler(DisplaySpanInfo(portalUuid)) { error: String, message: Any ->
+                js("displaySpanInfo(message.body);")
+            }
+
+            eb.publish(TracesTabOpened, "{'portal_uuid': '$portalUuid', 'trace_order_type': traceOrderType}")
+        };
+    }
+
     fun renderPage() {
         println("Rending Traces page")
         val root: Element = document.getElementById("root")!!
@@ -46,5 +78,49 @@ class TracesPage {
                 }
             }
         }
+    }
+
+    fun clickedDisplayTraceStack(appUuid: String, artifactQualifiedName: String, globalTraceId: String) {
+        eb.send(
+            ClickedDisplayTraceStack,
+            json(
+                "portal_uuid" to portalUuid,
+                "app_uuid" to appUuid,
+                "artifact_qualified_name" to artifactQualifiedName,
+                "trace_id" to globalTraceId
+            )
+        )
+    }
+
+    fun clickedDisplaySpanInfo(appUuid: String, rootArtifactQualifiedName: String, traceId: String, segmentId: String, spanId: Int) {
+        eb.send(
+            ClickedDisplaySpanInfo,
+            json(
+                "portal_uuid" to portalUuid,
+                "app_uuid" to appUuid,
+                "artifact_qualified_name" to rootArtifactQualifiedName,
+                "trace_id" to traceId,
+                "segment_id" to segmentId,
+                "span_id" to spanId
+            )
+        )
+    }
+
+    fun clickedBackToTraces() {
+        eb.send(
+            ClickedDisplayTraces,
+            json(
+                "portal_uuid" to portalUuid
+            )
+        )
+    }
+
+    fun clickedBackToTraceStack() {
+        eb.send(
+            ClickedDisplayTraceStack,
+            json(
+                "portal_uuid" to portalUuid
+            )
+        )
     }
 }
