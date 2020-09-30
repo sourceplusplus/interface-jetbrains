5a4a5b04a292f4189b9649518f12dc1af92de93d
chess-equality <chessequality@gmail.com>
2020-09-30 10:24:32 +0800

    [#251] Added moment.js; partial updates to traces
    Former-commit-id: 16eb871a5a41c60c019fee39adef1cc32fc8ea25

--- portal/frontend/build.gradle.kts
+++ portal/frontend/build.gradle.kts
@@ -17,6 +17,7 @@ kotlin {
 //                implementation(npm("sockjs-client", "1.4.0", generateExternals = true))
 //                implementation(npm("vertx3-eventbus-client", "3.9.1", generateExternals = true))
                 implementation(npm("jquery", "3.5.1", generateExternals = true))
+                implementation(npm("moment", "2.29.0"))
 
                 implementation(project(":protocol"))
                 implementation(kotlin("stdlib-common"))
