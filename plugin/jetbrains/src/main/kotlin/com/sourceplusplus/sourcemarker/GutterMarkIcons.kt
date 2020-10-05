c6ca132f114471ca87e089f157244167d70a8e72
Brandon Fergerson <bfergerson@apache.org>
2020-10-05 14:07:20 +0400

    clean up advice listener

---
+++ plugin/jetbrains/src/main/kotlin/com/sourceplusplus/sourcemarker/GutterMarkIcons.kt
@@ -0,0 +1,17 @@
+package com.sourceplusplus.sourcemarker
+
+import com.intellij.openapi.util.IconLoader
+import com.sourceplusplus.sourcemarker.listeners.PluginSourceMarkEventListener
+
+/**
+ * todo: description.
+ *
+ * @since 0.0.1
+ * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
+ */
+object GutterMarkIcons {
+    val exclamationTriangle = IconLoader.getIcon(
+        "/icons/exclamation-triangle.svg",
+        PluginSourceMarkEventListener::class.java
+    )
+}
--- plugin/jetbrains/src/main/kotlin/com/sourceplusplus/sourcemarker/listeners/PluginSourceMarkEventListener.kt
+++ plugin/jetbrains/src/main/kotlin/com/sourceplusplus/sourcemarker/listeners/PluginSourceMarkEventListener.kt
@@ -1,6 +1,5 @@
 package com.sourceplusplus.sourcemarker.listeners
 
-import com.intellij.openapi.util.IconLoader
 import com.sourceplusplus.marker.source.mark.api.MethodSourceMark
 import com.sourceplusplus.marker.source.mark.api.event.SourceMarkEvent
 import com.sourceplusplus.marker.source.mark.api.event.SourceMarkEventCode
@@ -19,11 +18,6 @@ import org.slf4j.LoggerFactory
  * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
  */
 class PluginSourceMarkEventListener(private val sourceMentor: SourceMentor) : SourceMarkEventListener {
-
-    private val exclamationTriangle = IconLoader.getIcon(
-        "/icons/exclamation-triangle.svg",
-        PluginSourceMarkEventListener::class.java
-    )
 
     companion object {
         private val log = LoggerFactory.getLogger(PluginSourceMarkEventListener::class.java)
