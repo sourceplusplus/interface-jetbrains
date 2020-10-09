15bd8d0b8afd89062a17978bdcd4940a4c05bad5
Brandon Fergerson <bfergerson@apache.org>
2020-10-09 10:48:50 +0400

    working on real overview page
    Former-commit-id: b9c1c96619d1fcf80beb0e74efdf6921b13e9841

--- protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/artifact/endpoint/EndpointResult.kt
+++ protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/artifact/endpoint/EndpointResult.kt
@@ -1,2 +1,16 @@
 package com.sourceplusplus.protocol.artifact.endpoint
 
+import com.sourceplusplus.protocol.portal.SplineChart
+
+/**
+ * todo: description.
+ *
+ * @since 0.0.1
+ * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
+ */
+data class EndpointResult(
+    val appUuid: String,
+    val start: Long, //todo: Instant
+    val stop: Long, //todo: Instant
+    val charts: List<SplineChart>
+)
