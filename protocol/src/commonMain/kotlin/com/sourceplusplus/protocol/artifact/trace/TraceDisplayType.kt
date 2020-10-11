473fad63987d3aaeb81aa2d7fdd699fa57bd6837
Brandon Fergerson <bfergerson@apache.org>
2020-10-11 16:50:29 +0400

    kdoc

--- protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/artifact/trace/TraceDisplayType.kt
+++ protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/artifact/trace/TraceDisplayType.kt
@@ -1,5 +1,11 @@
 package com.sourceplusplus.protocol.artifact.trace
 
+/**
+ * Represents the layers of a [Trace].
+ *
+ * @since 0.0.1
+ * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
+ */
 enum class TraceDisplayType {
     TRACES,
     TRACE_STACK,
