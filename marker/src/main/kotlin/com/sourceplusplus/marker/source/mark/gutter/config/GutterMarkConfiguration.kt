c0a696406f39238b59a240c7eca96ea62e58f65b
Brandon Fergerson <bfergerson@apache.org>
2020-10-13 09:06:50 +0400

    correct kdoc

--- marker/src/main/kotlin/com/sourceplusplus/marker/source/mark/gutter/config/GutterMarkConfiguration.kt
+++ marker/src/main/kotlin/com/sourceplusplus/marker/source/mark/gutter/config/GutterMarkConfiguration.kt
@@ -1,10 +1,10 @@
 package com.sourceplusplus.marker.source.mark.gutter.config
 
-import com.intellij.codeInsight.daemon.GutterMark
 import com.sourceplusplus.marker.source.mark.api.component.api.SourceMarkComponentProvider
 import com.sourceplusplus.marker.source.mark.api.component.jcef.SourceMarkJcefComponentProvider
 import com.sourceplusplus.marker.source.mark.api.config.SourceMarkConfiguration
 import com.sourceplusplus.marker.source.mark.api.filter.ApplySourceMarkFilter
+import com.sourceplusplus.marker.source.mark.gutter.GutterMark
 import javax.swing.Icon
 
 /**
