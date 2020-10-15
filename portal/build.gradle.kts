14f08c6a1ae8505198a68c61ceb94f148434ff53
Brandon Fergerson <bfergerson@apache.org>
2020-10-15 21:44:34 +0400

    fix missing field

--- portal/build.gradle.kts
+++ portal/build.gradle.kts
@@ -55,8 +55,6 @@ kotlin {
             dependencies {
                 implementation(npm("echarts", "4.8.0", generateExternals = true))
 //                implementation(npm("fomantic-ui-less", "2.8.6"))
-//                implementation(npm("sockjs-client", "1.4.0", generateExternals = true))
-//                implementation(npm("vertx3-eventbus-client", "3.9.2", generateExternals = true))
                 implementation(npm("jquery", "3.5.1", generateExternals = true))
                 implementation(npm("moment", "2.29.0", generateExternals = true))
 
@@ -65,7 +63,6 @@ kotlin {
                 implementation(kotlin("stdlib-js"))
                 implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.7.2")
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.1.0")
-//                implementation("com.github.hadilq:log4k-js:2.3.1")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.0")
                 implementation("com.github.bfergerson:kotlin-vertx3-eventbus-bridge:bacec93ae1")
             }
