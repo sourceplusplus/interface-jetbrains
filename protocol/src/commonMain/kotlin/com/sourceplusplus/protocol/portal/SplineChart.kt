14daae282b0cfe671d1de2dd98e200570e6980f4
Brandon Fergerson <bfergerson@apache.org>
2020-10-15 17:58:00 +0400

    finish js conversion of activity page

--- protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/portal/SplineChart.kt
+++ protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/portal/SplineChart.kt
@@ -1,11 +1,14 @@
 package com.sourceplusplus.protocol.portal
 
+import kotlinx.serialization.Serializable
+
 /**
  * todo: description.
  *
  * @since 0.0.1
  * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
  */
+@Serializable
 data class SplineChart(
     val metricType: MetricType,
     val timeFrame: QueryTimeFrame, //todo: use LocalDuration
