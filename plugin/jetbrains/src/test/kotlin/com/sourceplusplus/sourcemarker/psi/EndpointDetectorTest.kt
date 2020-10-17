418149e37c77533bb0f3900209b35a1fa2586743
Brandon Fergerson <bfergerson@apache.org>
2020-10-17 21:17:19 +0400

    kotlin endpoint name test
    Former-commit-id: 4436b0e0103823f06cd7ac2b49d8ca6bfea10298

---
+++ plugin/jetbrains/src/test/kotlin/com/sourceplusplus/sourcemarker/psi/EndpointDetectorTest.kt
@@ -0,0 +1,94 @@
+package com.sourceplusplus.sourcemarker.psi
+
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
+import org.junit.After
+import org.junit.Before
+
+open abstract class EndpointDetectorTest : LightJavaCodeInsightFixtureTestCase() {
+
+    @Before
+    public override fun setUp() {
+        super.setUp()
+
+        myFixture.addClass(
+            "package org.springframework.web.bind.annotation;\n" +
+                    "\n" +
+                    "import java.lang.annotation.Documented;\n" +
+                    "import java.lang.annotation.ElementType;\n" +
+                    "import java.lang.annotation.Retention;\n" +
+                    "import java.lang.annotation.RetentionPolicy;\n" +
+                    "import java.lang.annotation.Target;\n" +
+                    "\n" +
+                    "import org.springframework.core.annotation.AliasFor;\n" +
+                    "\n" +
+                    "@Target({ElementType.TYPE, ElementType.METHOD})\n" +
+                    "@Retention(RetentionPolicy.RUNTIME)\n" +
+                    "@Documented\n" +
+                    "@Mapping\n" +
+                    "public @interface RequestMapping {\n" +
+                    "\tString name() default \"\";\n" +
+                    "\t@AliasFor(\"path\")\n" +
+                    "\tString[] value() default {};\n" +
+                    "\t@AliasFor(\"value\")\n" +
+                    "\tString[] path() default {};\n" +
+                    "\tRequestMethod[] method() default {};\n" +
+                    "\tString[] params() default {};\n" +
+                    "\tString[] headers() default {};\n" +
+                    "\tString[] consumes() default {};\n" +
+                    "\tString[] produces() default {};\n" +
+                    "}\n"
+        )
+        myFixture.addClass(
+            "package org.springframework.web.bind.annotation;\n" +
+                    "\n" +
+                    "import java.lang.annotation.Documented;\n" +
+                    "import java.lang.annotation.ElementType;\n" +
+                    "import java.lang.annotation.Retention;\n" +
+                    "import java.lang.annotation.RetentionPolicy;\n" +
+                    "import java.lang.annotation.Target;\n" +
+                    "\n" +
+                    "import org.springframework.core.annotation.AliasFor;\n" +
+                    "\n" +
+                    "@Target(ElementType.METHOD)\n" +
+                    "@Retention(RetentionPolicy.RUNTIME)\n" +
+                    "@Documented\n" +
+                    "@RequestMapping(method = RequestMethod.GET)\n" +
+                    "public @interface GetMapping {\n" +
+                    "\n" +
+                    "\t@AliasFor(annotation = RequestMapping.class)\n" +
+                    "\tString name() default \"\";\n" +
+                    "\t@AliasFor(annotation = RequestMapping.class)\n" +
+                    "\tString[] value() default {};\n" +
+                    "\t@AliasFor(annotation = RequestMapping.class)\n" +
+                    "\tString[] path() default {};\n" +
+                    "\t@AliasFor(annotation = RequestMapping.class)\n" +
+                    "\tString[] params() default {};\n" +
+                    "\t@AliasFor(annotation = RequestMapping.class)\n" +
+                    "\tString[] headers() default {};\n" +
+                    "\t@AliasFor(annotation = RequestMapping.class)\n" +
+                    "\tString[] consumes() default {};\n" +
+                    "\t@AliasFor(annotation = RequestMapping.class)\n" +
+                    "\tString[] produces() default {};\n" +
+                    "}\n"
+        )
+        myFixture.addClass(
+            "package org.apache.skywalking.apm.toolkit.trace;\n" +
+                    "\n" +
+                    "import java.lang.annotation.ElementType;\n" +
+                    "import java.lang.annotation.Retention;\n" +
+                    "import java.lang.annotation.RetentionPolicy;\n" +
+                    "import java.lang.annotation.Target;\n" +
+                    "\n" +
+                    "@Target({ElementType.METHOD})\n" +
+                    "@Retention(RetentionPolicy.RUNTIME)\n" +
+                    "public @interface Trace {\n" +
+                    "    String operationName() default \"\";\n" +
+                    "}\n"
+        )
+    }
+
+    @After
+    public override fun tearDown() {
+        super.tearDown()
+    }
+}
