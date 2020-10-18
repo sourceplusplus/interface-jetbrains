08ccac2ed6b900dcff823861c8138c3279966908
Brandon Fergerson <bfergerson@apache.org>
2020-10-18 14:42:33 +0400

    scala endpoint name test
    Former-commit-id: 2f2950fe13855f6593cd6a678644d865eaf11ab4

--- plugin/jetbrains/src/test/kotlin/com/sourceplusplus/sourcemarker/psi/ScalaEndpointNameDetectorTest.kt
+++ plugin/jetbrains/src/test/kotlin/com/sourceplusplus/sourcemarker/psi/ScalaEndpointNameDetectorTest.kt
@@ -1,102 +1,104 @@
-//package com.sourceplusplus.sourcemarker.psi
-//
-//import com.intellij.ide.highlighter.JavaFileType
-//import com.intellij.psi.PsiFileFactory
-//import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixture4TestCase
-//import io.vertx.kotlin.coroutines.await
-//import kotlinx.coroutines.runBlocking
-//import org.intellij.lang.annotations.Language
-//import org.jetbrains.uast.UFile
-//import org.jetbrains.uast.toUElement
-//import org.junit.Test
-//
-//class ScalaEndpointNameDetectorTest : LightPlatformCodeInsightFixture4TestCase() {
-//
-//    @Test
-//    fun `SpringMVCEndpoint RequestMapping method`() {
-//        @Language("Scala") val code = """
-//                    class TestController {
-//                        @org.springframework.web.bind.annotation.RequestMapping(value = Array("/doGet"), method = Array(RequestMethod.GET))
-//                        def getStr() = {}
-//                    }
-//                """.trimIndent()
-//
-//        val uFile = PsiFileFactory.getInstance(project).createFileFromText(
-//            "TestController.scala", JavaFileType.INSTANCE, code
-//        ).toUElement() as UFile
-//        assertEquals(1, uFile.classes.size)
-//        assertEquals(1, uFile.classes[0].methods.size)
-//
-//        runBlocking {
-//            val result = EndpointDetector().determineEndpointName(uFile.classes[0].methods[0]).await()
-//            assertTrue(result.isPresent)
-//            assertEquals("{GET}/doGet", result.get())
-//        }
-//    }
-////
-////    @Test
-////    fun `SpringMVCEndpoint GetMapping method`() {
-////        @Language("Groovy") val code = """
-////                    class TestController {
-////                        @org.springframework.web.bind.annotation.GetMapping(name = "/doGet")
-////                        void doGet() {}
-////                    }
-////                """.trimIndent()
-////
-////        val uFile = PsiFileFactory.getInstance(project).createFileFromText(
-////            "TestController.groovy", JavaFileType.INSTANCE, code
-////        ).toUElement() as UFile
-////        assertEquals(1, uFile.classes.size)
-////        assertEquals(1, uFile.classes[0].methods.size)
-////
-////        runBlocking {
-////            val result = EndpointDetector().determineEndpointName(uFile.classes[0].methods[0]).await()
-////            assertTrue(result.isPresent)
-////            assertEquals("{GET}/doGet", result.get())
-////        }
-////    }
-////
-////    @Test
-////    fun `SkyWalking Trace with operation name`() {
-////        @Language("Groovy") val code = """
-////                    class TestController {
-////                        @org.apache.skywalking.apm.toolkit.trace.Trace(operationName = "doGet")
-////                        void doGet() {}
-////                    }
-////                """.trimIndent()
-////
-////        val uFile = PsiFileFactory.getInstance(project).createFileFromText(
-////            "TestController.groovy", JavaFileType.INSTANCE, code
-////        ).toUElement() as UFile
-////        assertEquals(1, uFile.classes.size)
-////        assertEquals(1, uFile.classes[0].methods.size)
-////
-////        runBlocking {
-////            val result = EndpointDetector().determineEndpointName(uFile.classes[0].methods[0]).await()
-////            assertTrue(result.isPresent)
-////            assertEquals("doGet", result.get())
-////        }
-////    }
-////
-////    @Test
-////    fun `SkyWalking Trace no operation name`() {
-////        @Language("Groovy") val code = """
-////                    class TestController {
-////                        @org.apache.skywalking.apm.toolkit.trace.Trace
-////                        void doGet() {}
-////                    }
-////                """.trimIndent()
-////
-////        val uFile = PsiFileFactory.getInstance(project).createFileFromText(
-////            "TestController.groovy", JavaFileType.INSTANCE, code
-////        ).toUElement() as UFile
-////        assertEquals(1, uFile.classes.size)
-////        assertEquals(1, uFile.classes[0].methods.size)
-////
-////        runBlocking {
-////            val result = EndpointDetector().determineEndpointName(uFile.classes[0].methods[0]).await()
-////            assertTrue(result.isPresent)
-////            assertEquals("TestController.doGet", result.get())
-////        }
-////    }
-//}
+package com.sourceplusplus.sourcemarker.psi
+
+import com.intellij.openapi.application.ApplicationManager
+import io.vertx.kotlin.coroutines.await
+import kotlinx.coroutines.runBlocking
+import org.intellij.lang.annotations.Language
+import org.jetbrains.uast.UFile
+import org.jetbrains.uast.toUElement
+import org.junit.Test
+
+class ScalaEndpointNameDetectorTest : EndpointDetectorTest() {
+
+    @Test
+    fun `SpringMVCEndpoint RequestMapping method`() {
+        @Language("Scala") val code = """
+                    import org.springframework.web.bind.annotation._
+                    class TestController {
+                        @RequestMapping(value = Array("/doGet"), method = Array(RequestMethod.GET))
+                        def getStr() {}
+                    }
+                """.trimIndent()
+        val uFile = myFixture.configureByText("TestController.scala", code).toUElement() as UFile
+
+        ApplicationManager.getApplication().runReadAction {
+            assertEquals(1, uFile.classes.size)
+            assertEquals(1, uFile.classes[0].methods.size)
+
+            runBlocking {
+                val result = EndpointDetector().determineEndpointName(uFile.classes[0].methods[0]).await()
+                assertTrue(result.isPresent)
+                assertEquals("{GET}/doGet", result.get())
+            }
+        }
+    }
+
+    @Test
+    fun `SpringMVCEndpoint GetMapping method`() {
+        @Language("Scala") val code = """
+                    import org.springframework.web.bind.annotation._
+                    class TestController {
+                        @GetMapping(name = "/doGet")
+                        def doGet() {}
+                    }
+                """.trimIndent()
+        val uFile = myFixture.configureByText("TestController.scala", code).toUElement() as UFile
+
+        ApplicationManager.getApplication().runReadAction {
+            assertEquals(1, uFile.classes.size)
+            assertEquals(1, uFile.classes[0].methods.size)
+
+            runBlocking {
+                val result = EndpointDetector().determineEndpointName(uFile.classes[0].methods[0]).await()
+                assertTrue(result.isPresent)
+                assertEquals("{GET}/doGet", result.get())
+            }
+        }
+    }
+
+    @Test
+    fun `SkyWalking Trace with operation name`() {
+        @Language("Scala") val code = """
+                    import org.apache.skywalking.apm.toolkit.trace.Trace
+                    class TestController {
+                        @Trace(operationName = "doGet")
+                        def doGet() {}
+                    }
+                """.trimIndent()
+        val uFile = myFixture.configureByText("TestController.scala", code).toUElement() as UFile
+
+        ApplicationManager.getApplication().runReadAction {
+            assertEquals(1, uFile.classes.size)
+            assertEquals(1, uFile.classes[0].methods.size)
+
+            runBlocking {
+                val result = EndpointDetector().determineEndpointName(uFile.classes[0].methods[0]).await()
+                assertTrue(result.isPresent)
+                assertEquals("doGet", result.get())
+            }
+        }
+    }
+
+    @Test
+    fun `SkyWalking Trace no operation name`() {
+        @Language("Scala") val code = """
+                    import org.apache.skywalking.apm.toolkit.trace.Trace
+                    class TestController {
+                        @Trace
+                        def doGet() {}
+                    }
+                """.trimIndent()
+        val uFile = myFixture.configureByText("TestController.scala", code).toUElement() as UFile
+
+        ApplicationManager.getApplication().runReadAction {
+            assertEquals(1, uFile.classes.size)
+            assertEquals(1, uFile.classes[0].methods.size)
+
+            runBlocking {
+                val result = EndpointDetector().determineEndpointName(uFile.classes[0].methods[0]).await()
+                assertTrue(result.isPresent)
+                assertEquals("TestController.doGet", result.get())
+            }
+        }
+    }
+}
