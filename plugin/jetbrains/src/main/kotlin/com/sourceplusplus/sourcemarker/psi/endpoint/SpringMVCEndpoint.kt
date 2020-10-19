10509afba9c18ed36c1da7c4cfa81fb520974614
Brandon Fergerson <bfergerson@apache.org>
2020-10-19 13:52:30 +0400

    #260 - split up by language
    Former-commit-id: 552231a28ec42325d52a5b853a1a469efe9cebde

--- plugin/jetbrains/src/main/kotlin/com/sourceplusplus/sourcemarker/psi/endpoint/SpringMVCEndpoint.kt
+++ plugin/jetbrains/src/main/kotlin/com/sourceplusplus/sourcemarker/psi/endpoint/SpringMVCEndpoint.kt
@@ -1,5 +1,6 @@
 package com.sourceplusplus.sourcemarker.psi.endpoint
 
+import com.intellij.lang.Language
 import com.intellij.openapi.application.ApplicationManager
 import com.sourceplusplus.sourcemarker.psi.EndpointDetector
 import io.vertx.core.Future
@@ -7,11 +8,10 @@ import io.vertx.core.Promise
 import org.jetbrains.plugins.groovy.lang.psi.uast.GrUReferenceExpression
 import org.jetbrains.plugins.scala.lang.psi.uast.expressions.ScUMethodCallExpression
 import org.jetbrains.plugins.scala.lang.psi.uast.expressions.ScUQualifiedReferenceExpression
+import org.jetbrains.uast.UAnnotation
 import org.jetbrains.uast.UMethod
 import org.jetbrains.uast.UQualifiedReferenceExpression
 import org.jetbrains.uast.expressions.UInjectionHost
-import org.jetbrains.uast.kotlin.KotlinAbstractUExpression
-import org.jetbrains.uast.kotlin.KotlinUAnnotation
 import org.jetbrains.uast.kotlin.KotlinUQualifiedReferenceExpression
 import org.jetbrains.uast.kotlin.expressions.KotlinUCollectionLiteralExpression
 import java.util.*
@@ -40,55 +40,19 @@ class SpringMVCEndpoint : EndpointDetector.EndpointNameDeterminer {
             for (annotationName in qualifiedNameSet) {
                 val annotation = uMethod.findAnnotation(annotationName)
                 if (annotation != null) {
-                    if (annotationName == requestMappingAnnotation) {
-                        val endpointNameExpr = annotation.attributeValues.find { it.name == "value" }!!.expression
-                        val methodExpr = annotation.attributeValues.find { it.name == "method" }!!.expression
-                        if (endpointNameExpr is KotlinAbstractUExpression) {
-                            val value = if (endpointNameExpr is KotlinUCollectionLiteralExpression) {
-                                endpointNameExpr.valueArguments[0].evaluate()
-                            } else {
-                                endpointNameExpr.evaluate()
-                            }
-                            val method =
-                                ((methodExpr as KotlinUCollectionLiteralExpression)
-                                    .valueArguments[0] as KotlinUQualifiedReferenceExpression)
-                                    .selector.asSourceString()
-                            promise.complete(Optional.of("{$method}$value"))
-                        } else if (endpointNameExpr is ScUMethodCallExpression) {
-                            val value = endpointNameExpr.valueArguments[0].evaluate()
-                            val method = ((methodExpr as ScUMethodCallExpression).valueArguments[0]
-                                    as ScUQualifiedReferenceExpression).resolve()!!.text
-                            promise.complete(Optional.of("{$method}$value"))
-                        } else {
-                            val value = (endpointNameExpr as UInjectionHost).evaluateToString()
-                            val method = if (methodExpr is UQualifiedReferenceExpression) {
-                                methodExpr.selector.toString()
-                            } else {
-                                (methodExpr as GrUReferenceExpression).resolvedName.toString()
-                            }
-                            promise.complete(Optional.of("{$method}$value"))
+                    when {
+                        annotation.lang === Language.findLanguageByID("JAVA")
+                                || annotation.lang === Language.findLanguageByID("Groovy") -> {
+                            promise.complete(handleJavaOrGroovyAnnotation(annotation, annotationName))
                         }
-                    } else {
-                        if (annotation is KotlinUAnnotation) {
-                            val valueExpr = annotation.findAttributeValue("value")!!
-                            val value = if (valueExpr is KotlinUCollectionLiteralExpression) {
-                                valueExpr.valueArguments[0].evaluate()
-                            } else {
-                                valueExpr.evaluate()
-                            }
-                            val method = annotationName.substring(annotationName.lastIndexOf(".") + 1)
-                                .replace("Mapping", "").toUpperCase()
-                            promise.complete(Optional.of("{$method}$value"))
-                        } else {
-                            val endpointNameExpr = annotation.attributeValues.find { it.name == "name" }!!
-                            val value = if (endpointNameExpr is UInjectionHost) {
-                                endpointNameExpr.evaluateToString()
-                            } else {
-                                endpointNameExpr.evaluate()
-                            } as String
-                            val method = annotationName.substring(annotationName.lastIndexOf(".") + 1)
-                                .replace("Mapping", "").toUpperCase()
-                            promise.complete(Optional.of("{$method}$value"))
+                        annotation.lang === Language.findLanguageByID("kotlin") -> {
+                            promise.complete(handleKotlinAnnotation(annotation, annotationName))
+                        }
+                        annotation.lang === Language.findLanguageByID("Scala") -> {
+                            promise.complete(handleScalaAnnotation(annotation, annotationName))
+                        }
+                        else -> {
+                            throw UnsupportedOperationException("Language ${annotation.lang} is not currently supported")
                         }
                     }
                 }
@@ -97,5 +61,76 @@ class SpringMVCEndpoint : EndpointDetector.EndpointNameDeterminer {
             promise.tryComplete(Optional.empty())
         }
         return promise.future()
+    }
+
+    private fun handleJavaOrGroovyAnnotation(annotation: UAnnotation, annotationName: String): Optional<String> {
+        if (annotationName == requestMappingAnnotation) {
+            val endpointNameExpr = annotation.attributeValues.find { it.name == "value" }!!.expression
+            val methodExpr = annotation.attributeValues.find { it.name == "method" }!!.expression
+            val value = (endpointNameExpr as UInjectionHost).evaluateToString()
+            val method = if (methodExpr is UQualifiedReferenceExpression) {
+                methodExpr.selector.toString()
+            } else {
+                (methodExpr as GrUReferenceExpression).resolvedName.toString()
+            }
+            return Optional.of("{$method}$value")
+        } else {
+            val endpointNameExpr = annotation.attributeValues.find { it.name == "name" }!!
+            val value = if (endpointNameExpr is UInjectionHost) {
+                endpointNameExpr.evaluateToString()
+            } else {
+                endpointNameExpr.evaluate()
+            } as String
+            val method = annotationName.substring(annotationName.lastIndexOf(".") + 1)
+                .replace("Mapping", "").toUpperCase()
+            return Optional.of("{$method}$value")
+        }
+    }
+
+    private fun handleKotlinAnnotation(annotation: UAnnotation, annotationName: String): Optional<String> {
+        if (annotationName == requestMappingAnnotation) {
+            val endpointNameExpr = annotation.attributeValues.find { it.name == "value" }!!.expression
+            val methodExpr = annotation.attributeValues.find { it.name == "method" }!!.expression
+            val value = if (endpointNameExpr is KotlinUCollectionLiteralExpression) {
+                endpointNameExpr.valueArguments[0].evaluate()
+            } else {
+                endpointNameExpr.evaluate()
+            }
+            val method = ((methodExpr as KotlinUCollectionLiteralExpression)
+                .valueArguments[0] as KotlinUQualifiedReferenceExpression)
+                .selector.asSourceString()
+            return Optional.of("{$method}$value")
+        } else {
+            val valueExpr = annotation.findAttributeValue("value")!!
+            val value = if (valueExpr is KotlinUCollectionLiteralExpression) {
+                valueExpr.valueArguments[0].evaluate()
+            } else {
+                valueExpr.evaluate()
+            }
+            val method = annotationName.substring(annotationName.lastIndexOf(".") + 1)
+                .replace("Mapping", "").toUpperCase()
+            return Optional.of("{$method}$value")
+        }
+    }
+
+    private fun handleScalaAnnotation(annotation: UAnnotation, annotationName: String): Optional<String> {
+        if (annotationName == requestMappingAnnotation) {
+            val endpointNameExpr = annotation.attributeValues.find { it.name == "value" }!!.expression
+            val methodExpr = annotation.attributeValues.find { it.name == "method" }!!.expression
+            val value = (endpointNameExpr as ScUMethodCallExpression).valueArguments[0].evaluate()
+            val method = ((methodExpr as ScUMethodCallExpression).valueArguments[0]
+                    as ScUQualifiedReferenceExpression).resolve()!!.text
+            return Optional.of("{$method}$value")
+        } else {
+            val endpointNameExpr = annotation.attributeValues.find { it.name == "name" }!!
+            val value = if (endpointNameExpr is UInjectionHost) {
+                endpointNameExpr.evaluateToString()
+            } else {
+                endpointNameExpr.evaluate()
+            } as String
+            val method = annotationName.substring(annotationName.lastIndexOf(".") + 1)
+                .replace("Mapping", "").toUpperCase()
+            return Optional.of("{$method}$value")
+        }
     }
 }
