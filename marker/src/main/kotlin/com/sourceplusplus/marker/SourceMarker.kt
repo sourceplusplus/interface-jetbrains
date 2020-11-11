3bebff73fdc40998961368ef6ae262980f50e72d
Brandon Fergerson <bfergerson@apache.org>
2020-11-10 20:08:23 -0500

    unnecessary
    Former-commit-id: 13694500cc8c7f5d923f00e7ce015bd7415c7adb

--- marker/src/main/kotlin/com/sourceplusplus/marker/SourceMarker.kt
+++ marker/src/main/kotlin/com/sourceplusplus/marker/SourceMarker.kt
@@ -15,7 +15,6 @@ import org.slf4j.LoggerFactory
  * @since 0.1.0
  * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
  */
-//todo: should be per project
 object SourceMarker {
 
     @Volatile
