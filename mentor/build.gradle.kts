c23c713e2fa15a0284a0eccaac3112af735cf947
Brandon Fergerson <bfergerson@apache.org>
2021-01-19 12:21:58 -0500

    publishing group/artifact/version info

--- mentor/build.gradle.kts
+++ mentor/build.gradle.kts
@@ -1,5 +1,20 @@
 plugins {
     id("org.jetbrains.kotlin.jvm")
+    id("maven-publish")
+}
+
+val pluginGroup: String by project
+val mentorVersion: String by project
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = pluginGroup
+            artifactId = "mentor"
+            version = mentorVersion
+
+            from(components["java"])
+        }
+    }
 }
 
 dependencies {
