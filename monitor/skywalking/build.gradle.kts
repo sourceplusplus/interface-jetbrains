c46b6374dd3ff516b557722617bbe1cb99e8bee1
Brandon Fergerson <bfergerson@apache.org>
2021-02-02 19:30:03 -0500

    dependency upgrades

--- monitor/skywalking/build.gradle.kts
+++ monitor/skywalking/build.gradle.kts
@@ -1,21 +1,21 @@
 plugins {
     id("org.jetbrains.kotlin.jvm")
-    id("com.apollographql.apollo").version("2.4.4")
+    id("com.apollographql.apollo").version("2.5.3")
 }
 
 dependencies {
     implementation(project(":protocol"))
     implementation("org.slf4j:slf4j-api:1.7.30")
-    implementation("com.apollographql.apollo:apollo-runtime:2.4.4")
-    implementation("com.apollographql.apollo:apollo-coroutines-support:2.4.4")
-    api("com.apollographql.apollo:apollo-api:2.4.4")
+    implementation("com.apollographql.apollo:apollo-runtime:2.5.3")
+    implementation("com.apollographql.apollo:apollo-coroutines-support:2.5.3")
+    api("com.apollographql.apollo:apollo-api:2.5.3")
 
     val vertxVersion = "4.0.0"
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
     implementation("io.vertx:vertx-core:$vertxVersion")
     implementation("io.vertx:vertx-lang-kotlin:$vertxVersion")
     implementation("io.vertx:vertx-lang-kotlin-coroutines:$vertxVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.1.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.1.1")
 }
 
 apollo {
