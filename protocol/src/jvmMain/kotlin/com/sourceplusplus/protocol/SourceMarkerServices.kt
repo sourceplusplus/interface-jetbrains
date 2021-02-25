37d5c54f2eb48530e6246d18908b12c96f0f26e9
Brandon Fergerson <bfergerson@apache.org>
2021-02-24 22:21:47 -0500

    uniform

--- protocol/src/jvmMain/kotlin/com/sourceplusplus/protocol/SourceMarkerServices.kt
+++ protocol/src/jvmMain/kotlin/com/sourceplusplus/protocol/SourceMarkerServices.kt
@@ -1,11 +1,11 @@
 package com.sourceplusplus.protocol
 
 object SourceMarkerServices {
-    const val TRACING_SERVICES = "tracing.service:"
-    const val LOGGING_SERVICES = "logging.service:"
+    const val TRACING_SERVICES = "sm.tracing.service"
+    const val LOGGING_SERVICES = "sm.logging.service"
 
     object Provider {
-        const val LOG_COUNT_INDICATOR = LOGGING_SERVICES + "log-count-indicator"
-        const val LOCAL_TRACING = TRACING_SERVICES + "local-tracing"
+        const val LOG_COUNT_INDICATOR = "$LOGGING_SERVICES.log-count-indicator"
+        const val LOCAL_TRACING = "$TRACING_SERVICES.local-tracing"
     }
 }
