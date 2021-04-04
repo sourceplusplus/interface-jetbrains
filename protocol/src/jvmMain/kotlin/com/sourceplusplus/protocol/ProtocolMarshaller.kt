f375b7741c437c67dddb4e41a97887f98ca3c7db
Brandon Fergerson <bfergerson@apache.org>
2021-04-04 00:00:09 -0400

    setup

--- protocol/src/jvmMain/kotlin/com/sourceplusplus/protocol/ProtocolMarshaller.kt
+++ protocol/src/jvmMain/kotlin/com/sourceplusplus/protocol/ProtocolMarshaller.kt
@@ -7,14 +7,19 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.sourceplusplus.protocol.artifact.ArtifactQualifiedName
+import com.sourceplusplus.protocol.artifact.debugger.BreakpointHit
 import com.sourceplusplus.protocol.artifact.debugger.LiveBreakpoint
 import com.sourceplusplus.protocol.artifact.debugger.SourceLocation
 import com.sourceplusplus.protocol.artifact.log.LogCountSummary
 import com.sourceplusplus.protocol.artifact.trace.TraceResult
+import io.vertx.core.Vertx
+import io.vertx.core.buffer.Buffer
+import io.vertx.core.eventbus.MessageCodec
 import io.vertx.core.json.Json
 import io.vertx.core.json.JsonObject
 import io.vertx.core.json.jackson.DatabindCodec
 import kotlinx.datetime.Instant
+import java.util.*
 
 /**
  * todo: description.
@@ -94,5 +99,33 @@ object ProtocolMarshaller {
     @JvmStatic
     fun deserializeLocation(value: JsonObject): SourceLocation {
         return value.mapTo(SourceLocation::class.java)
+    }
+
+    @JvmStatic
+    fun setupCodecs(vertx: Vertx) {
+        vertx.eventBus().registerDefaultCodec(BreakpointHit::class.java, ProtocolMessageCodec())
+    }
+
+    class ProtocolMessageCodec<T> : MessageCodec<T, T> {
+        override fun encodeToWire(buffer: Buffer?, s: T?) {
+            val value = Json.encode(s).toByteArray()
+            buffer?.appendInt(value.size)
+            buffer?.appendBytes(value)
+        }
+
+        override fun decodeFromWire(pos: Int, buffer: Buffer?): T? {
+            val len = buffer?.getInt(pos) ?: return null
+            val bytes = buffer.getBytes(pos + 4, pos + 4 + len)
+            return cast(Json.decodeValue(String(bytes)))
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        private fun <T> cast(obj: Any?): T? {
+            return obj as? T
+        }
+
+        override fun transform(o: T): T = o
+        override fun name(): String = UUID.randomUUID().toString()
+        override fun systemCodecID(): Byte = -1
     }
 }
