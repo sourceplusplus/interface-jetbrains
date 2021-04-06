0523055519649d2b4c6f22678254949f07b14c04
Brandon Fergerson <bfergerson@apache.org>
2021-04-05 20:06:49 -0400

    test

--- protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/artifact/exception/JvmStackTraceElement.kt
+++ protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/artifact/exception/JvmStackTraceElement.kt
@@ -3,6 +3,7 @@ package com.sourceplusplus.protocol.artifact.exception
 import com.sourceplusplus.protocol.artifact.debugger.TraceVariable
 import com.sourceplusplus.protocol.utils.ArtifactNameUtils.getShortQualifiedClassName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 /**
  * todo: description.
@@ -11,29 +12,39 @@ import kotlinx.serialization.Serializable
  * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
  */
 @Serializable
+@Suppress("TRANSIENT_IS_REDUNDANT")
 data class JvmStackTraceElement(
     val method: String,
     val source: String,
     val variables: MutableList<TraceVariable> = mutableListOf()
 ) {
+    @Transient
     val sourceAsFilename: String?
         get() = if (source.contains(":")) {
             source.substring(0, source.indexOf(":"))
         } else {
             null
         }
+
+    @Transient
     val sourceAsLineNumber: Int?
         get() = if (source.contains(":")) {
             source.substring(source.indexOf(":") + 1).toInt()
         } else {
             null
         }
+
+    @Transient
     val qualifiedClassName: String by lazy {
         method.substring(0, method.lastIndexOf("."))
     }
+
+    @Transient
     val shortQualifiedClassName: String by lazy {
         getShortQualifiedClassName(qualifiedClassName)
     }
+
+    @Transient
     val methodName: String by lazy {
         method.substring(method.lastIndexOf(".") + 1)
     }
