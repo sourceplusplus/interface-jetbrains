6e5ebe7225784b070e324f4c98b974dfd631dc24
Brandon Fergerson <bfergerson@apache.org>
2021-04-05 20:15:33 -0400

    didn't work

--- protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/artifact/exception/JvmStackTraceElement.kt
+++ protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/artifact/exception/JvmStackTraceElement.kt
@@ -12,39 +12,29 @@ import kotlinx.serialization.Transient
  * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
  */
 @Serializable
-@Suppress("TRANSIENT_IS_REDUNDANT")
 data class JvmStackTraceElement(
     val method: String,
     val source: String,
     val variables: MutableList<TraceVariable> = mutableListOf()
 ) {
-    @Transient
     val sourceAsFilename: String?
         get() = if (source.contains(":")) {
             source.substring(0, source.indexOf(":"))
         } else {
             null
         }
-
-    @Transient
     val sourceAsLineNumber: Int?
         get() = if (source.contains(":")) {
             source.substring(source.indexOf(":") + 1).toInt()
         } else {
             null
         }
-
-    @Transient
     val qualifiedClassName: String by lazy {
         method.substring(0, method.lastIndexOf("."))
     }
-
-    @Transient
     val shortQualifiedClassName: String by lazy {
         getShortQualifiedClassName(qualifiedClassName)
     }
-
-    @Transient
     val methodName: String by lazy {
         method.substring(method.lastIndexOf(".") + 1)
     }
