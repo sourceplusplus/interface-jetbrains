88426e107025b4290f144c4425f174177a820ebf
Brandon Fergerson <brandon.fergerson@codebrig.com>
2021-05-09 23:08:28 -0400

    version bumps
    Former-commit-id: ae99bbd84e83aa0f5e6d297d6bea9591e61efd8f

--- monitor/skywalking/build.gradle.kts
+++ monitor/skywalking/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     implementation("com.apollographql.apollo:apollo-coroutines-support:2.5.6")
     api("com.apollographql.apollo:apollo-api:2.5.6")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3-native-mt")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0-RC-native-mt")
     implementation("io.vertx:vertx-core:$vertxVersion")
     implementation("io.vertx:vertx-lang-kotlin:$vertxVersion")
     implementation("io.vertx:vertx-lang-kotlin-coroutines:$vertxVersion")
