db1188bee5bb841d07825bdb7ab0d0670884ad83
Brandon Fergerson <bfergerson@apache.org>
2021-06-17 11:47:55 -0400

    kotlin bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ repositories {
 subprojects {
     ext {
         set("vertxVersion", "4.0.3")
-        set("kotlinVersion", "1.5.0")
+        set("kotlinVersion", "1.5.10")
     }
 
     repositories {
