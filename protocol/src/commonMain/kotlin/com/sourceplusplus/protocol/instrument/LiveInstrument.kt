fb398aa86d29906fc2c78d21ce3a7048a8979899
Brandon Fergerson <bfergerson@apache.org>
2021-06-17 14:46:04 -0400

    more config

--- protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/instrument/LiveInstrument.kt
+++ protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/instrument/LiveInstrument.kt
@@ -17,5 +17,7 @@ abstract class LiveInstrument {
     abstract val id: String?
     abstract val type: LiveInstrumentType
     open var applyImmediately: Boolean = false
+    open var applied: Boolean = false
     open var pending: Boolean = false
+    open var hitRateLimit: Int = 1000 //limit of once per X milliseconds
 }
