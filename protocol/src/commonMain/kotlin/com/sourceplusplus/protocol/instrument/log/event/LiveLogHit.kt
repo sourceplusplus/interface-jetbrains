cd01e89033112f03711a104641ae3ee56419e60a
Brandon Fergerson <bfergerson@apache.org>
2021-06-20 16:19:48 -0400

    live log hit

---
+++ protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/instrument/log/event/LiveLogHit.kt
@@ -0,0 +1,25 @@
+package com.sourceplusplus.protocol.instrument.log.event
+
+import com.sourceplusplus.protocol.Serializers
+import com.sourceplusplus.protocol.artifact.log.LogResult
+import com.sourceplusplus.protocol.instrument.LiveInstrumentEventType
+import kotlinx.datetime.Instant
+import kotlinx.serialization.Serializable
+
+/**
+ * todo: description.
+ *
+ * @since 0.2.2
+ * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
+ */
+@Serializable
+data class LiveLogHit(
+    val logId: String,
+    @Serializable(with = Serializers.InstantKSerializer::class)
+    val occurredAt: Instant,
+    val host: String,
+    val application: String,
+    val logResult: LogResult
+) {
+    val eventType: LiveInstrumentEventType = LiveInstrumentEventType.LOG_HIT
+}
