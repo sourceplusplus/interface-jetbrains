638b8343b6dbb745d40d036e0f4096f827353e27
Brandon Fergerson <bfergerson@apache.org>
2021-06-25 20:06:28 -0400

    final

--- protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/view/LiveViewSubscription.kt
+++ protocol/src/commonMain/kotlin/com/sourceplusplus/protocol/view/LiveViewSubscription.kt
@@ -11,6 +11,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class LiveViewSubscription(
     val subscriptionId: String? = null,
+    val entityId: String? = null,
     val artifactQualifiedName: String,
     val metricNames: List<String>,
     val type: ViewSubscriptionType,
