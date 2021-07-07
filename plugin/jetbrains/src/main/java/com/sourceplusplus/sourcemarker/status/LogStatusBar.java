ad5fa4adc9b03de53e8910e849e4f0d9e4bf798e
Brandon Fergerson <bfergerson@apache.org>
2021-07-07 19:55:18 -0400

    ui wip

--- plugin/jetbrains/src/main/java/com/sourceplusplus/sourcemarker/status/LogStatusBar.java
+++ plugin/jetbrains/src/main/java/com/sourceplusplus/sourcemarker/status/LogStatusBar.java
@@ -5,6 +5,9 @@ import com.intellij.openapi.editor.impl.EditorComponentImpl;
 import com.intellij.openapi.util.IconLoader;
 import com.intellij.util.ui.UIUtil;
 import com.sourceplusplus.marker.source.mark.inlay.InlayMark;
+import com.sourceplusplus.protocol.artifact.log.Log;
+import com.sourceplusplus.protocol.instrument.LiveSourceLocation;
+import com.sourceplusplus.protocol.instrument.log.LiveLog;
 import com.sourceplusplus.sourcemarker.AutocompleteField;
 import com.sourceplusplus.sourcemarker.command.CommandBarController;
 import net.miginfocom.swing.MigLayout;
@@ -13,36 +16,65 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.*;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static com.sourceplusplus.sourcemarker.status.ViewUtils.containsScreenLocation;
 
 public class LogStatusBar extends JPanel {
 
-    private final List<String> commands = Stream.of(
-            "/add-live-log", "/add-live-breakpoint"
-
-//todo: impl, don't show clears if results in no-op
-//            ,"/clear-live-logs", "/clear-live-breakpoints", "/clear-live-instruments"
-    ).sorted().collect(Collectors.toList());
-    private final Function<String, List<String>> lookup = text -> commands.stream()
-            .filter(v -> !text.isEmpty()
-                    && v.toLowerCase().contains(text.toLowerCase().replace("/", ""))
-                    && text.startsWith("/")
-                    && !v.equalsIgnoreCase(text)
-            ).collect(Collectors.toList());
-
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("hh:mm:ss a")
+            .withZone(ZoneId.systemDefault());
     private final InlayMark inlayMark;
+    private final LiveSourceLocation sourceLocation;
+    private final List<String> scopeVars;
+    private final Function<String, List<String>> lookup;
+    private final Pattern VARIABLE_PATTERN;
     private Editor editor;
+    private LiveLog liveLog;
 
-    public LogStatusBar(InlayMark inlayMark) {
+    public LogStatusBar(LiveSourceLocation sourceLocation, List<String> scopeVars, InlayMark inlayMark) {
+        this(sourceLocation, scopeVars, inlayMark, null, null);
+    }
+
+    public LogStatusBar(LiveSourceLocation sourceLocation, List<String> scopeVars, InlayMark inlayMark,
+                        LiveLog liveLog, Editor editor) {
+        this.sourceLocation = sourceLocation;
+        this.scopeVars = scopeVars;
+        lookup = text -> scopeVars.stream()
+                .filter(v -> {
+                    String var = substringAfterLast(" ", text);
+                    return var.startsWith("$") && v.toLowerCase().contains(var.substring(1))
+                            && !v.toLowerCase().equals(var.substring(1));
+                })
+                .limit(7)
+                .collect(Collectors.toList());
+
+        StringBuilder sb = new StringBuilder("(");
+        for (int i = 0; i < scopeVars.size(); i++) {
+            sb.append("\\$").append(scopeVars.get(i));
+            if (i + 1 < scopeVars.size()) {
+                sb.append("|");
+            }
+        }
+        sb.append(")(?:\\s|$)");
+        VARIABLE_PATTERN = Pattern.compile(sb.toString());
+
         this.inlayMark = inlayMark;
+        this.liveLog = liveLog;
+        this.editor = editor;
 
         initComponents();
         setupComponents();
+    }
+
+    public void setLatestLog(Instant time, Log latestLog) {
+
     }
 
     public void setEditor(Editor editor) {
@@ -136,6 +168,12 @@ public class LogStatusBar extends JPanel {
         label2.setIcon(IconLoader.getIcon("/icons/closeIcon.svg"));
     }
 
+    public static String substringAfterLast(String delimiter, String value) {
+        int index = value.lastIndexOf(delimiter);
+        if (index == -1) return value;
+        else return value.substring(index + delimiter.length());
+    }
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - unknown
@@ -143,7 +181,8 @@ public class LogStatusBar extends JPanel {
         label1 = new JLabel();
         label3 = new JLabel();
         label4 = new JLabel();
-        textField1 = new AutocompleteField("Search or Type a Command (/)", lookup);
+        separator1 = new JSeparator();
+        textField1 = new AutocompleteField("Input log message (use $ for variables)", lookup);
         label2 = new JLabel();
 
         //======== this ========
@@ -151,12 +190,12 @@ public class LogStatusBar extends JPanel {
         setMinimumSize(new Dimension(500, 40));
         setBorder(new LineBorder(new Color(85, 85, 85)));
         setBackground(new Color(43, 43, 43));
-//        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border. EmptyBorder
-//        ( 0, 0, 0, 0) , "JF\u006frmDes\u0069gner \u0045valua\u0074ion", javax. swing. border. TitledBorder. CENTER, javax. swing. border
-//        . TitledBorder. BOTTOM, new java .awt .Font ("D\u0069alog" ,java .awt .Font .BOLD ,12 ), java. awt
-//        . Color. red) , getBorder( )) );  addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void
-//        propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062order" .equals (e .getPropertyName () )) throw new RuntimeException( )
-//        ; }} );
+//        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border .
+//        EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn" , javax. swing .border . TitledBorder. CENTER ,javax . swing
+//        . border .TitledBorder . BOTTOM, new java. awt .Font ( "Dia\u006cog", java .awt . Font. BOLD ,12 ) ,
+//        java . awt. Color .red ) , getBorder () ) );  addPropertyChangeListener( new java. beans .PropertyChangeListener ( )
+//        { @Override public void propertyChange (java . beans. PropertyChangeEvent e) { if( "\u0062ord\u0065r" .equals ( e. getPropertyName () ) )
+//        throw new RuntimeException( ) ;} } );
         setLayout(new MigLayout(
             "hidemode 3",
             // columns
@@ -194,7 +233,14 @@ public class LogStatusBar extends JPanel {
         //---- label4 ----
         label4.setIcon(IconLoader.getIcon("/icons/clock.svg"));
         label4.setText("time goes here");
-        add(label4, "cell 1 0");
+        add(label4, "cell 1 0,gapx null 8");
+
+        //---- separator1 ----
+        separator1.setPreferredSize(new Dimension(5, 20));
+        separator1.setMinimumSize(new Dimension(5, 20));
+        separator1.setOrientation(SwingConstants.VERTICAL);
+        separator1.setMaximumSize(new Dimension(5, 20));
+        add(separator1, "cell 1 0");
 
         //---- textField1 ----
         textField1.setBackground(new Color(37, 37, 37));
@@ -214,6 +260,7 @@ public class LogStatusBar extends JPanel {
     private JLabel label1;
     private JLabel label3;
     private JLabel label4;
+    private JSeparator separator1;
     private JTextField textField1;
     private JLabel label2;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
