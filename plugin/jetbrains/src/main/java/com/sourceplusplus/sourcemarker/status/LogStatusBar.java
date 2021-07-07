43bde303f83f644e1213c6927379c7aaf3380bf2
Brandon Fergerson <bfergerson@apache.org>
2021-07-07 19:28:03 -0400

    ui wip

--- plugin/jetbrains/src/main/java/com/sourceplusplus/sourcemarker/status/LogStatusBar.java
+++ plugin/jetbrains/src/main/java/com/sourceplusplus/sourcemarker/status/LogStatusBar.java
@@ -139,7 +139,10 @@ public class LogStatusBar extends JPanel {
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - unknown
+        panel1 = new JPanel();
         label1 = new JLabel();
+        label3 = new JLabel();
+        label4 = new JLabel();
         textField1 = new AutocompleteField("Search or Type a Command (/)", lookup);
         label2 = new JLabel();
 
@@ -148,35 +151,70 @@ public class LogStatusBar extends JPanel {
         setMinimumSize(new Dimension(500, 40));
         setBorder(new LineBorder(new Color(85, 85, 85)));
         setBackground(new Color(43, 43, 43));
+//        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing. border. EmptyBorder
+//        ( 0, 0, 0, 0) , "JF\u006frmDes\u0069gner \u0045valua\u0074ion", javax. swing. border. TitledBorder. CENTER, javax. swing. border
+//        . TitledBorder. BOTTOM, new java .awt .Font ("D\u0069alog" ,java .awt .Font .BOLD ,12 ), java. awt
+//        . Color. red) , getBorder( )) );  addPropertyChangeListener (new java. beans. PropertyChangeListener( ){ @Override public void
+//        propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062order" .equals (e .getPropertyName () )) throw new RuntimeException( )
+//        ; }} );
         setLayout(new MigLayout(
             "hidemode 3",
             // columns
+            "0[fill]" +
             "[fill]" +
             "[grow,fill]" +
             "[fill]",
             // rows
             "0[grow]0"));
 
-        //---- label1 ----
-        label1.setIcon(IconLoader.getIcon("/icons/command/logo.svg"));
-        add(label1, "cell 0 0");
+        //======== panel1 ========
+        {
+            panel1.setBackground(new Color(37, 37, 37));
+            panel1.setPreferredSize(null);
+            panel1.setMinimumSize(null);
+            panel1.setMaximumSize(null);
+            panel1.setLayout(new MigLayout(
+                "fill,insets 0,hidemode 3",
+                // columns
+                "5[fill]" +
+                "[fill]4",
+                // rows
+                "[grow]"));
+
+            //---- label1 ----
+            label1.setIcon(IconLoader.getIcon("/icons/align-left.svg"));
+            panel1.add(label1, "cell 0 0");
+
+            //---- label3 ----
+            label3.setIcon(IconLoader.getIcon("/icons/angle-down.svg"));
+            panel1.add(label3, "cell 1 0");
+        }
+        add(panel1, "cell 0 0, grow");
+
+        //---- label4 ----
+        label4.setIcon(IconLoader.getIcon("/icons/clock.svg"));
+        label4.setText("time goes here");
+        add(label4, "cell 1 0");
 
         //---- textField1 ----
         textField1.setBackground(new Color(37, 37, 37));
         textField1.setBorder(new LineBorder(Color.darkGray, 1, true));
         textField1.setFont(new Font("Roboto Light", Font.PLAIN, 14));
-        add(textField1, "cell 1 0");
+        add(textField1, "cell 2 0");
 
         //---- label2 ----
         label2.setIcon(IconLoader.getIcon("/icons/closeIcon.svg"));
-        add(label2, "cell 2 0");
+        add(label2, "cell 3 0");
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - unknown
+    private JPanel panel1;
     private JLabel label1;
-    private AutocompleteField textField1;
+    private JLabel label3;
+    private JLabel label4;
+    private JTextField textField1;
     private JLabel label2;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
