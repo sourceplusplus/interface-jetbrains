9e4ae76aeb983a58dae2435648eceeeeea0182bf
Brandon Fergerson <brandon.fergerson@codebrig.com>
2021-10-03 15:03:12 -0400

    Update SourceFileMarker.kt

--- marker/src/main/kotlin/com/sourceplusplus/marker/source/SourceFileMarker.kt
+++ marker/src/main/kotlin/com/sourceplusplus/marker/source/SourceFileMarker.kt
@@ -17,6 +17,12 @@ import com.sourceplusplus.marker.source.mark.gutter.ExpressionGutterMark
 import com.sourceplusplus.marker.source.mark.gutter.MethodGutterMark
 import com.sourceplusplus.marker.source.mark.inlay.ExpressionInlayMark
 import com.sourceplusplus.marker.source.mark.inlay.MethodInlayMark
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.plugins.groovy.lang.psi.GroovyFile
+import org.jetbrains.uast.UClass
+import org.jetbrains.uast.UExpression
+import org.jetbrains.uast.UMethod
+import org.jetbrains.uast.getContainingUMethod
 import org.slf4j.LoggerFactory
 import java.util.*
 
