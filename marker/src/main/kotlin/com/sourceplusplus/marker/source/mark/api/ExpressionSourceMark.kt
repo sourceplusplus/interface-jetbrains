5153cbc06691d7aa58f76cd2cc1bd7e404233205
Brandon Fergerson <bfergerson@apache.org>
2021-10-04 18:15:30 -0400

    clean

--- marker/src/main/kotlin/com/sourceplusplus/marker/source/mark/api/ExpressionSourceMark.kt
+++ marker/src/main/kotlin/com/sourceplusplus/marker/source/mark/api/ExpressionSourceMark.kt
@@ -87,13 +87,6 @@ abstract class ExpressionSourceMark(
     }
 
     override fun getPsiElement(): PsiElement {
-        //todo: move to jvm
-//        if (psiExpression is UDeclarationsExpression) {
-//            //todo: support for multi-declaration statements
-//            return (psiExpression as UDeclarationsExpression).declarations[0].sourcePsi!!
-//        } else {
-//            return psiExpression.sourcePsi!!
-//        }
         return psiExpression
     }
 
