3dfa634103ee68644a79a539a896bcfa61024c2a
Brandon Fergerson <brandon.fergerson@codebrig.com>
2021-10-17 10:34:00 -0400

    bump apollo (#525)

--- monitor/skywalking/build.gradle.kts
+++ monitor/skywalking/build.gradle.kts
@@ -1,17 +1,18 @@
 plugins {
     id("org.jetbrains.kotlin.jvm")
-    id("com.apollographql.apollo").version("2.5.9")
+    id("com.apollographql.apollo").version("2.5.10")
 }
 
 val vertxVersion = ext.get("vertxVersion")
 val kotlinVersion = ext.get("kotlinVersion")
+val apolloVersion: String by project
 
 dependencies {
     implementation(project(":protocol"))
     implementation("org.slf4j:slf4j-api:1.7.32")
-    implementation("com.apollographql.apollo:apollo-runtime:2.5.9")
-    implementation("com.apollographql.apollo:apollo-coroutines-support:2.5.9")
-    api("com.apollographql.apollo:apollo-api:2.5.9")
+    implementation("com.apollographql.apollo:apollo-runtime:$apolloVersion")
+    implementation("com.apollographql.apollo:apollo-coroutines-support:$apolloVersion")
+    api("com.apollographql.apollo:apollo-api:$apolloVersion")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinVersion")
     implementation("io.vertx:vertx-core:$vertxVersion")
