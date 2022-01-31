7e29650c90eb5c7dd39b08839aa8aea8880ce76d
Brandon Fergerson <bfergerson@apache.org>
2022-01-31 13:16:55 -0500

    add license

--- plugin/src/test/kotlin/spp/jetbrains/sourcemarker/psi/LoggerDetectorTest.kt
+++ plugin/src/test/kotlin/spp/jetbrains/sourcemarker/psi/LoggerDetectorTest.kt
@@ -1,5 +1,23 @@
-//package com.sourceplusplus.sourcemarker.psi
-//
+/*
+ * Source++, the open-source live coding platform.
+ * Copyright (C) 2022 CodeBrig, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package spp.jetbrains.sourcemarker.psi
+
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 //import com.intellij.ide.highlighter.JavaFileType
 //import com.intellij.openapi.application.ApplicationManager
 //import com.intellij.openapi.module.Module
@@ -27,7 +45,7 @@
 //import org.junit.jupiter.api.Test
 //import java.io.File
 //
-//class LoggerDetectorTest : LightJavaCodeInsightFixtureTestCase() {
+class LoggerDetectorTest : LightJavaCodeInsightFixtureTestCase() {
 //
 //    override fun getProjectDescriptor(): LightProjectDescriptor {
 //        return object : DefaultLightProjectDescriptor() {
@@ -133,4 +151,4 @@
 //            }
 //        }
 //    }
-//}
+}
