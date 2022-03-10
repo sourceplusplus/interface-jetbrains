f43f48fee330bb4939e19d73d73d0cb2f7a8c360
Brandon Fergerson <bfergerson@apache.org>
2022-03-10 16:57:44 +0100

    bump

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
         implementation(project(":marker:jvm-marker"))
         implementation(project(":marker:py-marker"))
         implementation(project(":monitor"))
-        implementation("com.github.sourceplusplus.interface-portal:portal-jvm:4cd858772b") { isTransitive = false }
+        implementation("com.github.sourceplusplus.interface-portal:portal-jvm:4d03b0b769") { isTransitive = false }
         implementation("com.github.sourceplusplus.protocol:protocol:a2cd759830")
     }
 
