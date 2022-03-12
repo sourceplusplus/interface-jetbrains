701de365e7ce90d7530aaefda09a95d301854d47
Brandon Fergerson <bfergerson@apache.org>
2022-03-12 18:01:05 +0100

    bump

--- marker/build.gradle.kts
+++ marker/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     val intellijVersion = "213.6777.52"
 
     compileOnly("org.jooq:joor:$joorVersion")
-    compileOnly("com.github.sourceplusplus.protocol:protocol:a2cd759830")
+    compileOnly("com.github.sourceplusplus.protocol:protocol:5ab12b5c51")
     compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinVersion")
     compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     compileOnly("com.google.guava:guava:31.0.1-jre")
