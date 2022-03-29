06eb61deac73bd9d9dc3a4773a6e5186ee57c57b
Brandon Fergerson <bfergerson@apache.org>
2022-03-29 20:24:36 +0200

    hardcode

--- monitor/build.gradle.kts
+++ monitor/build.gradle.kts
@@ -10,7 +10,7 @@ val projectVersion: String by project
 val slf4jVersion: String by project
 
 dependencies {
-    implementation("com.github.sourceplusplus.protocol:protocol:$projectVersion")
+    implementation("com.github.sourceplusplus.protocol:protocol:0.4.4")
     implementation("org.slf4j:slf4j-api:$slf4jVersion")
     implementation("com.apollographql.apollo3:apollo-runtime:$apolloVersion")
     api("com.apollographql.apollo3:apollo-api:$apolloVersion")
