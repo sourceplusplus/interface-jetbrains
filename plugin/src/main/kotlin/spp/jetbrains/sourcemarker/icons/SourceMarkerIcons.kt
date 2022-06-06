d7939c8cb7c8652fc34600c1e5c19634844b415e
Brandon Fergerson <bfergerson@apache.org>
2022-06-06 11:59:54 +0400

    IU-222.2889.14 compatibility issue

--- plugin/src/main/kotlin/spp/jetbrains/sourcemarker/icons/SourceMarkerIcons.kt
+++ plugin/src/main/kotlin/spp/jetbrains/sourcemarker/icons/SourceMarkerIcons.kt
@@ -17,12 +17,7 @@
  */
 package spp.jetbrains.sourcemarker.icons
 
-import com.intellij.ui.scale.ScaleContext
-import com.intellij.util.SVGLoader
-import com.intellij.util.ui.JBImageIcon
 import spp.jetbrains.sourcemarker.PluginIcons
-import java.io.ByteArrayInputStream
-import javax.swing.Icon
 
 /**
  * Defines the various visual icons SourceMarker may display.
@@ -32,25 +27,9 @@ import javax.swing.Icon
  */
 object SourceMarkerIcons {
 
-    val exclamationTriangle = PluginIcons.exclamationTriangle
-    val performanceRamp = PluginIcons.performanceRamp
-    val activeException = PluginIcons.activeException
     val LIVE_METER_COUNT_ICON = PluginIcons.count
     val LIVE_METER_GAUGE_ICON = PluginIcons.gauge
     val LIVE_METER_HISTOGRAM_ICON = PluginIcons.histogram
     val LIVE_BREAKPOINT_ACTIVE_ICON = PluginIcons.Breakpoint.active
     val LIVE_BREAKPOINT_DISABLED_ICON = PluginIcons.Breakpoint.disabled
-    val LIVE_BREAKPOINT_COMPLETE_ICON = PluginIcons.Breakpoint.complete
-    val LIVE_BREAKPOINT_PENDING_ICON = PluginIcons.Breakpoint.pending
-    val LIVE_BREAKPOINT_ERROR_ICON = PluginIcons.Breakpoint.error
-
-    fun getNumericGutterMarkIcon(value: Int, color: String = "#182d34"): Icon {
-        return JBImageIcon(
-            SVGLoader.loadHiDPI(
-                null,
-                ByteArrayInputStream(NumericSvgIcon(value, color).toString().toByteArray()),
-                ScaleContext.create()
-            )
-        )
-    }
 }
