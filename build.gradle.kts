f83fc814e750e05b560ce897cc1095c0e8895e43
Brandon Fergerson <bfergerson@apache.org>
2022-06-23 12:21:26 +0400

    us gh release asset instead of gh package asset

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,13 +83,8 @@ tasks {
         doLast {
             val f = File(projectDir, "test/e2e/spp-probe-$projectVersion.jar")
             if (!f.exists()) {
-                println("Fetching Source++ JVM probe metadata")
-                val depUrl = "https://pkg.sourceplus.plus/sourceplusplus/probe-jvm/plus/sourceplus/probe/probe-jvm/${project.version}"
-                val timestamp = Regex("<timestamp>([0-9\\.]+)<\\/timestamp>")
-                    .find(java.net.URL("$depUrl/maven-metadata.xml").readText())!!.groupValues[1]
-                val versionNum = project.version.toString().replace("-SNAPSHOT", "")
                 println("Downloading Source++ JVM probe")
-                java.net.URL("$depUrl/probe-jvm-$versionNum-$timestamp-1.jar")
+                java.net.URL("https://github.com/sourceplusplus/probe-jvm/releases/download/${project.version}/spp-probe-${project.version}.jar")
                     .openStream().use { input ->
                         java.io.FileOutputStream(f).use { output ->
                             input.copyTo(output)
