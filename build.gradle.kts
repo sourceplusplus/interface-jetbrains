f77abb31884640bb296805b3882dcd32ad4d067c
Brandon Fergerson <bfergerson@apache.org>
2022-08-09 12:07:41 +0400

    test: gh publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,25 +32,27 @@ subprojects {
         maven(url = "https://pkg.sourceplus.plus/sourceplusplus/interface-booster-ui")
     }
 
-    apply(plugin = "app.cash.licensee")
-    configure<app.cash.licensee.LicenseeExtension> {
-        ignoreDependencies("plus.sourceplus", "protocol")
-        ignoreDependencies("plus.sourceplus", "protocol-jvm")
-        ignoreDependencies("plus.sourceplus.interface", "interface-booster-ui")
-        allow("Apache-2.0")
-        allow("MIT")
-        allow("EPL-1.0")
-        allow("LGPL-2.1-only")
-        allowUrl("https://raw.githubusercontent.com/apollographql/apollo-kotlin/main/LICENSE") //MIT
-        allowUrl("https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html") //LGPL 2.1
-        allowUrl("https://www.bouncycastle.org/licence.html") //MIT
-        allowUrl("https://api.github.com/licenses/apache-2.0") //Apache 2.0
-        allowUrl("http://www.jcraft.com/jsch/LICENSE.txt") //BSD-style
-        allowUrl("http://www.jcraft.com/jzlib/LICENSE.txt") //BSD-style
-        allowUrl("http://jgrapht.org/LGPL.html") //LGPL 2.1
-        allowUrl("http://www.eclipse.org/legal/epl-v20.html") //EPL 2.0
-        allowDependency("net.jcip", "jcip-annotations", "1.0") {
-            because("Creative Commons")
+    if (!this.toString().contains("commander")) {
+        apply(plugin = "app.cash.licensee")
+        configure<app.cash.licensee.LicenseeExtension> {
+            ignoreDependencies("plus.sourceplus", "protocol")
+            ignoreDependencies("plus.sourceplus", "protocol-jvm")
+            ignoreDependencies("plus.sourceplus.interface", "interface-booster-ui")
+            allow("Apache-2.0")
+            allow("MIT")
+            allow("EPL-1.0")
+            allow("LGPL-2.1-only")
+            allowUrl("https://raw.githubusercontent.com/apollographql/apollo-kotlin/main/LICENSE") //MIT
+            allowUrl("https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html") //LGPL 2.1
+            allowUrl("https://www.bouncycastle.org/licence.html") //MIT
+            allowUrl("https://api.github.com/licenses/apache-2.0") //Apache 2.0
+            allowUrl("http://www.jcraft.com/jsch/LICENSE.txt") //BSD-style
+            allowUrl("http://www.jcraft.com/jzlib/LICENSE.txt") //BSD-style
+            allowUrl("http://jgrapht.org/LGPL.html") //LGPL 2.1
+            allowUrl("http://www.eclipse.org/legal/epl-v20.html") //EPL 2.0
+            allowDependency("net.jcip", "jcip-annotations", "1.0") {
+                because("Creative Commons")
+            }
         }
     }
 
