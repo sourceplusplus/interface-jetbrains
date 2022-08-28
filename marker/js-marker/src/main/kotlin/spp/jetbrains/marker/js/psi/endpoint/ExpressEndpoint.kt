bc2b4ee6857b4611c65a0a4f54e91ed6fae861c7
UltraDev <petzmagnus@gmail.com>
2022-08-28 10:47:21 +0200

    Fix express endpoint detection

--- marker/js-marker/src/main/kotlin/spp/jetbrains/marker/js/psi/endpoint/ExpressEndpoint.kt
+++ marker/js-marker/src/main/kotlin/spp/jetbrains/marker/js/psi/endpoint/ExpressEndpoint.kt
@@ -16,11 +16,10 @@
  */
 package spp.jetbrains.marker.js.psi.endpoint
 
-import com.intellij.lang.javascript.psi.JSCallExpression
-import com.intellij.lang.javascript.psi.JSLiteralExpression
-import com.intellij.lang.javascript.psi.JSReferenceExpression
-import com.intellij.lang.javascript.psi.JSVariable
+import com.intellij.lang.javascript.psi.*
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.psi.PsiElement
+import com.intellij.psi.search.searches.ReferencesSearch
 import io.vertx.core.Future
 import io.vertx.core.Promise
 import spp.jetbrains.marker.source.info.EndpointDetector
@@ -51,73 +50,144 @@ class ExpressEndpoint : EndpointDetector.EndpointNameDeterminer {
             val method = expression.firstChild as JSReferenceExpression
             val router = method.firstChild as JSReferenceExpression
             val routerVariable = router.resolve() as JSVariable
-            if (routerVariable.initializer !is JSCallExpression) {
+
+            if (method.children.size < 3) {
                 promise.complete(Optional.empty())
                 return@runReadAction
             }
-            val initializer = routerVariable.initializer as JSCallExpression
-
-            var isRouter = false
-            if (initializer.isRequireCall) {
-                if (initializer.arguments[0] !is JSLiteralExpression) { // TODO: How would we support require with a variable?
-                    promise.complete(Optional.empty())
-                    return@runReadAction
-                }
-
-                val require = initializer.arguments[0] as JSLiteralExpression
-                if (require.value != "express") {
-                    promise.complete(Optional.empty())
-                    return@runReadAction
-                }
-            } else {
-                val initializerMethod = initializer.firstChild
-                if (initializerMethod.children[2].text != "Router") { // TODO: Is this the only thing we want out of express?
-                    promise.complete(Optional.empty())
-                    return@runReadAction
-                }
-
-                val expressReference = initializerMethod.children[0] as JSReferenceExpression
-                val expressVariable = expressReference.resolve() as JSVariable
-                if (expressVariable.initializer !is JSCallExpression) {
-                    promise.complete(Optional.empty())
-                    return@runReadAction
-                }
-
-                val expressInitializer = expressVariable.initializer as JSCallExpression
-                if (!expressInitializer.isRequireCall) {
-                    promise.complete(Optional.empty())
-                    return@runReadAction
-                }
-                if (expressInitializer.arguments[0] !is JSLiteralExpression) { // TODO: How would we support require with a variable?
-                    promise.complete(Optional.empty())
-                    return@runReadAction
-                }
-
-                val expressRequire = expressInitializer.arguments[0] as JSLiteralExpression
-                if (expressRequire.value != "express") { // TODO: Is this the only way to get express?
-                    promise.complete(Optional.empty())
-                    return@runReadAction
-                }
-
-                isRouter = true
-            }
-
             val endpointType = method.children[2].text
             val endpointName = expression.arguments[0].text
-
-            if (isRouter) {
-                // TODO: Find usages of routerVariable
-            }
 
             if (endpointType == "get" ||
                 endpointType == "post" ||
                 endpointType == "put" ||
-                endpointType == "delete") {
-                promise.complete(Optional.of(DetectedEndpoint(endpointName, false, type = endpointType.toUpperCase())))
+                endpointType == "delete"
+            ) {
+                val basePath = locateRouter(routerVariable)
+                if (basePath == null) {
+                    promise.complete(Optional.empty())
+                    return@runReadAction
+                }
+
+                promise.complete(
+                    Optional.of(
+                        DetectedEndpoint(
+                            basePath + endpointName,
+                            false,
+                            type = endpointType.toUpperCase()
+                        )
+                    )
+                )
             } else {
                 promise.complete(Optional.empty())
             }
         }
         return promise.future()
+    }
+
+    private fun locateRouter(routerVariable: JSVariable): String? {
+        if (isExpressApp(routerVariable)) {
+            return ""
+        }
+
+        if (routerVariable.initializer !is JSCallExpression) {
+            return null
+        }
+        val initializer = routerVariable.initializer as JSCallExpression
+
+        val initializerMethod = initializer.firstChild
+        if (initializerMethod.children[2].text != "Router") { // TODO: Is this the only thing we want out of express?
+            return null
+        }
+
+        val expressReference = initializerMethod.children[0] as JSReferenceExpression
+        val expressVariable = expressReference.resolve() as JSVariable
+        if (expressVariable.initializer !is JSCallExpression) {
+            return null
+        }
+
+        val expressInitializer = expressVariable.initializer as JSCallExpression
+        if (!expressInitializer.isRequireCall) {
+            return null
+        }
+        if (expressInitializer.arguments[0] !is JSLiteralExpression) { // TODO: How would we support require with a variable?
+            return null
+        }
+
+        val expressRequire = expressInitializer.arguments[0] as JSLiteralExpression
+        if (expressRequire.value != "express") { // TODO: Is this the only way to get express?
+            return null
+        }
+
+        val routes = ReferencesSearch.search(routerVariable).map {
+            if (it.element.parent !is JSArgumentList) {
+                return@map null
+            }
+            val argumentList = it.element.parent as JSArgumentList
+            if (argumentList.arguments.size != 2) { // TODO: Is there any situation where this isn't the case?
+                return@map null
+            }
+            if (argumentList.arguments[1] != it.element) {
+                return@map null
+            }
+
+            val callExpression = it.element.parent.parent
+            val useReference = callExpression.firstChild as JSReferenceExpression
+            val superRouter = (useReference.firstChild as JSReferenceExpression).resolve() as JSVariable
+            val superPath = locateRouter(superRouter)
+            if (superPath == null) {
+                return@map null
+            }
+
+            val pathExpression = argumentList.arguments[0] as JSLiteralExpression
+
+            return@map superPath + pathExpression.value
+        }.filter { it != null }.map { it!! }
+
+        if (routes.size > 1) {
+            // TODO: Handle multiple routes
+        }
+
+        return routes[0]
+    }
+
+    private fun isExpressApp(element: PsiElement): Boolean {
+        if (element is JSVariable) {
+            if (element.initializer !is JSCallExpression) {
+                return false
+            }
+            val initializer = element.initializer as JSCallExpression
+            return isExpress(initializer.firstChild)
+        }
+        if (element is JSReferenceExpression) {
+            val resolved = element.resolve() ?: return false
+            return isExpressApp(resolved)
+        }
+        return false
+    }
+
+    private fun isExpress(element: PsiElement): Boolean {
+        if (element is JSVariable) {
+            val initializer = element.initializer
+            if (initializer !is JSCallExpression) {
+                return false
+            }
+
+            if (initializer.arguments[0] !is JSLiteralExpression) { // TODO: How would we support require with a variable?
+                return false
+            }
+
+            val require = initializer.arguments[0] as JSLiteralExpression
+            if (require.value != "express") {
+                return false
+            }
+
+            return true
+        }
+        if (element is JSReferenceExpression) {
+            val resolved = element.resolve() ?: return false
+            return isExpress(resolved)
+        }
+        return false
     }
 }
