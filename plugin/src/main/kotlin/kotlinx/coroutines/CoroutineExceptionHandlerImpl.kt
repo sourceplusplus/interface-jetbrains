1fd5750a9bceb548ed40de2b2438539bc8e27a1a
Brandon Fergerson <bfergerson@apache.org>
2022-08-30 03:20:31 +0400

    remove from build

--- plugin/src/main/kotlin/kotlinx/coroutines/CoroutineExceptionHandlerImpl.kt
+++
@@ -1,7 +0,0 @@
-package kotlinx.coroutines
-
-import kotlin.coroutines.CoroutineContext
-
-fun handleCoroutineExceptionImpl(context: CoroutineContext, exception: Throwable) {
-    exception.printStackTrace();
-}
