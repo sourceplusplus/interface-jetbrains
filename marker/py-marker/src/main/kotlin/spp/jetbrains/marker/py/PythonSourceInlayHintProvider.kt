57e663474bc8d41417cd8119fa0ee32587a5801c
Brandon Fergerson <bfergerson@apache.org>
2022-09-11 16:40:00 +0400

    feat: impl python displayVirtualText

--- marker/py-marker/src/main/kotlin/spp/jetbrains/marker/py/PythonSourceInlayHintProvider.kt
+++ marker/py-marker/src/main/kotlin/spp/jetbrains/marker/py/PythonSourceInlayHintProvider.kt
@@ -19,6 +19,9 @@ package spp.jetbrains.marker.py
 import com.intellij.codeInsight.hints.InlayHintsSink
 import com.intellij.codeInsight.hints.presentation.InlayPresentation
 import com.intellij.psi.PsiElement
+import com.jetbrains.python.psi.PyCallExpression
+import com.jetbrains.python.psi.PyFunction
+import com.jetbrains.python.psi.PyStatement
 import spp.jetbrains.marker.plugin.SourceInlayHintProvider
 import spp.jetbrains.marker.source.mark.inlay.InlayMark
 import spp.jetbrains.marker.source.mark.inlay.config.InlayMarkVirtualText
@@ -41,6 +44,46 @@ class PythonSourceInlayHintProvider : SourceInlayHintProvider() {
         sink: InlayHintsSink,
         representation: InlayPresentation
     ) {
-        TODO("Not yet implemented")
+        var statement = if (element is PyStatement) element else element
+        if (virtualText.useInlinePresentation) {
+            if (virtualText.showAfterLastChildWhenInline) {
+                if (statement is PyCallExpression) {
+                    statement = statement.parent //todo: more dynamic
+                }
+                sink.addInlineElement(
+                    statement.lastChild.textRange.endOffset,
+                    virtualText.relatesToPrecedingText,
+                    representation
+                )
+            } else {
+                sink.addInlineElement(
+                    statement.textRange.startOffset,
+                    virtualText.relatesToPrecedingText,
+                    representation
+                )
+            }
+        } else {
+            if (statement.parent is PyFunction) {
+                virtualText.spacingTillMethodText = statement.parent.prevSibling.text
+                    .replace("\n", "").count { it == ' ' }
+            }
+
+            var startOffset = statement.textRange.startOffset
+            if (virtualText.showBeforeAnnotationsWhenBlock) {
+                if (statement.parent is PyFunction) {
+                    val annotations = (statement.parent as PyFunction).decoratorList?.decorators ?: emptyArray()
+                    if (annotations.isNotEmpty()) {
+                        startOffset = annotations[0].textRange.startOffset
+                    }
+                }
+            }
+            sink.addBlockElement(
+                startOffset,
+                virtualText.relatesToPrecedingText,
+                virtualText.showAbove,
+                0,
+                representation
+            )
+        }
     }
 }
