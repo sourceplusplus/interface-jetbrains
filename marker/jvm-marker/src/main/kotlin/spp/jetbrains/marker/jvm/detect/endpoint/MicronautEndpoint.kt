0ff6b1201d0a7bd318eec0794a4b21a5c9b1bbd6
Brandon Fergerson <bfergerson@apache.org>
2022-10-11 19:25:33 +0400

    feat: micronaut endpoint detector

---
+++ marker/jvm-marker/src/main/kotlin/spp/jetbrains/marker/jvm/detect/endpoint/MicronautEndpoint.kt
@@ -0,0 +1,92 @@
+/*
+ * Source++, the open-source live coding platform.
+ * Copyright (C) 2022 CodeBrig, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package spp.jetbrains.marker.jvm.detect.endpoint
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.util.Computable
+import io.vertx.core.Future
+import io.vertx.core.Promise
+import org.jetbrains.uast.UAnnotation
+import org.jetbrains.uast.UMethod
+import org.jetbrains.uast.expressions.UInjectionHost
+import org.jetbrains.uast.toUElementOfType
+import spp.jetbrains.marker.jvm.detect.JVMEndpointDetector.JVMEndpointNameDeterminer
+import spp.jetbrains.marker.source.info.EndpointDetector.DetectedEndpoint
+import spp.jetbrains.marker.source.mark.guide.GuideMark
+import java.util.*
+
+/**
+ * Detects Micronaut HTTP Server endpoints.
+ *
+ * @since 0.7.2
+ * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
+ */
+class MicronautEndpoint : JVMEndpointNameDeterminer {
+
+    private val log = logger<MicronautEndpoint>()
+    private val endpointAnnotationPrefix = "io.micronaut.http.annotation."
+
+    override fun determineEndpointName(uMethod: UMethod): Future<Optional<DetectedEndpoint>> {
+        val promise = Promise.promise<Optional<DetectedEndpoint>>()
+        ApplicationManager.getApplication().runReadAction {
+            val annotation = uMethod.annotations.find {
+                it.qualifiedName?.startsWith(endpointAnnotationPrefix) == true
+            }.toUElementOfType<UAnnotation>()
+            if (annotation != null) {
+                val endpointValue = annotation.attributeValues.find { it.name == null }
+                val value = if (endpointValue is UInjectionHost) {
+                    endpointValue.evaluateToString()
+                } else {
+                    endpointValue?.evaluate()
+                } as String?
+
+                //get controller
+                val controllerAnnotation = uMethod.containingClass?.annotations?.find {
+                    it.qualifiedName?.startsWith(endpointAnnotationPrefix) == true
+                }?.toUElementOfType<UAnnotation>()
+                val controllerValue = controllerAnnotation?.attributeValues?.find { it.name == null }
+                val controller = if (controllerValue is UInjectionHost) {
+                    controllerValue.evaluateToString()
+                } else {
+                    controllerValue?.evaluate()
+                } as String?
+
+                val endpoint = if (controller != null) "$controller$value" else value
+                if (endpoint?.isNotBlank() == true) {
+                    log.info("Detected Micronaut endpoint: $endpoint")
+                    promise.complete(Optional.of(DetectedEndpoint(endpoint, false)))
+                }
+            }
+
+            promise.tryComplete(Optional.empty())
+        }
+        return promise.future()
+    }
+
+    override fun determineEndpointName(guideMark: GuideMark): Future<Optional<DetectedEndpoint>> {
+        if (!guideMark.isMethodMark) {
+            return Future.succeededFuture(Optional.empty())
+        }
+
+        return ApplicationManager.getApplication().runReadAction(Computable {
+            val uMethod = guideMark.getPsiElement().toUElementOfType<UMethod>()
+                ?: return@Computable Future.succeededFuture(Optional.empty())
+            determineEndpointName(uMethod)
+        })
+    }
+}
