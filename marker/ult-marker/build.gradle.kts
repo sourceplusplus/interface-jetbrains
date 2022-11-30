6eeea593099d43baff61a8dbf1be4db1a55890ff
Brandon Fergerson <bfergerson@apache.org>
2022-11-30 17:38:28 +0400

    chore: interface package naming

--- marker/ult-marker/build.gradle.kts
+++ marker/ult-marker/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm")
+    id("maven-publish")
 }
 
 val vertxVersion: String by project
@@ -8,6 +9,39 @@ val protocolVersion = project.properties["protocolVersion"] as String? ?: projec
 
 intellij {
     type.set("IU")
+}
+
+val sourcesJar = tasks.register<Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    from(project.the<SourceSetContainer>()["main"].allSource)
+}
+
+configure<PublishingExtension> {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/sourceplusplus/interface-jetbrains")
+            credentials {
+                username = System.getenv("GH_PUBLISH_USERNAME")?.toString()
+                password = System.getenv("GH_PUBLISH_TOKEN")?.toString()
+            }
+        }
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.group.toString()
+                artifactId = "jetbrains-marker-ult"
+                version = project.version.toString()
+
+                from(components["kotlin"])
+
+                // Ship the sources jar
+                artifact(sourcesJar)
+            }
+        }
+    }
 }
 
 dependencies {
