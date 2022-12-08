b136d71a1f65eb2f495b7091aeed622965c6cf2d
Brandon Fergerson <bfergerson@apache.org>
2022-12-08 10:38:24 +0400

    chore: fix compatability issue

--- marker/src/main/kotlin/spp/jetbrains/marker/service/ArtifactVersionService.kt
+++ marker/src/main/kotlin/spp/jetbrains/marker/service/ArtifactVersionService.kt
@@ -19,15 +19,31 @@ package spp.jetbrains.marker.service
 import com.intellij.codeInsight.actions.VcsFacadeImpl
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.progress.util.ProgressIndicatorUtils
 import com.intellij.openapi.project.DumbService
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.util.ThrowableComputable
+import com.intellij.openapi.util.text.StringUtilRt
+import com.intellij.openapi.vcs.VcsException
+import com.intellij.openapi.vcs.changes.Change
+import com.intellij.openapi.vcs.changes.ChangeListChange
 import com.intellij.openapi.vcs.changes.ChangeListManager
+import com.intellij.openapi.vcs.changes.CurrentContentRevision
+import com.intellij.openapi.vcs.ex.LocalRange
+import com.intellij.openapi.vcs.ex.PartialLocalLineStatusTracker
+import com.intellij.openapi.vcs.ex.Range
+import com.intellij.openapi.vcs.ex.createRanges
+import com.intellij.openapi.vcs.impl.LineStatusTrackerManager
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.*
+import com.intellij.util.Function
 import com.intellij.util.SmartList
 import com.intellij.util.concurrency.FutureResult
 import com.intellij.util.containers.ContainerUtil
+import java.util.*
 
 /**
  * Responsible for determining the changes/versions of source code artifacts.
@@ -48,7 +64,7 @@ object ArtifactVersionService {
                 .getChangesIn(psiFile.virtualFile).toTypedArray()
             try {
                 val changedFunctions = ReadAction.compute(ThrowableComputable {
-                    VcsFacadeImpl.getVcsInstance().getChangedElements(project, changes) {
+                    getChangedElements(project, changes) {
                         if (DumbService.isDumb(project) || project.isDisposed || !it.isValid) {
                             return@getChangedElements emptyList()
                         }
@@ -83,5 +99,119 @@ object ArtifactVersionService {
         }
 
         return ProgressIndicatorUtils.awaitWithCheckCanceled(ref)
+    }
+
+    /**
+     * Taken from [VcsFacadeImpl.getChangedElements]
+     */
+    private fun <T : PsiElement> getChangedElements(
+        project: Project,
+        changes: Array<Change>,
+        elementsConvertor: Function<in VirtualFile, out List<T>>
+    ): List<T> {
+        val result: MutableList<T> = SmartList()
+        for (change in changes) {
+            if (change.type == Change.Type.DELETED) continue
+            if (change.afterRevision !is CurrentContentRevision) continue
+            val file = (change.afterRevision as CurrentContentRevision?)!!.virtualFile ?: continue
+            val document = FileDocumentManager.getInstance().getDocument(file) ?: continue
+            val apply = elementsConvertor.`fun`(file)
+            val elements = if (apply == null) null else ContainerUtil.skipNulls(apply)
+            if (ContainerUtil.isEmpty(elements)) continue
+            val changedLines = getChangedLines(project, document, change)
+            if (changedLines != null) {
+                for (element in elements!!) {
+                    if (isElementChanged(element, document, changedLines)) {
+                        result.add(element)
+                    }
+                }
+            } else {
+                result.addAll(elements!!)
+            }
+        }
+        return result
+    }
+
+    /**
+     * Taken from [VcsFacadeImpl.getChangedLines]
+     */
+    private fun getChangedLines(project: Project, document: Document, change: Change): BitSet? {
+        if (change.type == Change.Type.NEW) return null
+        val ranges = getChangedRanges(project, document, change) ?: return null
+        val changedLines = BitSet()
+        for (range in ranges) {
+            if (!range.hasLines()) {
+                if (range.hasVcsLines()) {
+                    changedLines[0.coerceAtLeast(range.line1 - 1)] = range.line1 + 1
+                }
+            } else {
+                changedLines[range.line1] = range.line2
+            }
+        }
+        return changedLines
+    }
+
+    /**
+     * Taken from [VcsFacadeImpl.getChangedRanges]
+     */
+    private fun getChangedRanges(project: Project, document: Document, change: Change): List<Range>? {
+        val tracker = LineStatusTrackerManager.getInstance(project).getLineStatusTracker(document)
+        return if (tracker != null) {
+            if (change is ChangeListChange && tracker is PartialLocalLineStatusTracker) {
+                val changeListId = change.changeListId
+                val ranges = tracker.getRanges()
+                if (ranges != null) {
+                    ContainerUtil.filter(ranges) { range: LocalRange -> range.changelistId == changeListId }
+                } else {
+                    null
+                }
+            } else {
+                tracker.getRanges()
+            }
+        } else {
+            val contentFromVcs = getRevisionedContentFrom(change)
+            if (contentFromVcs != null) {
+                getRanges(document, contentFromVcs)
+            } else {
+                null
+            }
+        }
+    }
+
+    /**
+     * Taken from [VcsFacadeImpl.isElementChanged]
+     */
+    private fun isElementChanged(element: PsiElement, document: Document, changedLines: BitSet): Boolean {
+        val textRange = element.textRange
+        val startLine = document.getLineNumber(textRange.startOffset)
+        val endLine = if (textRange.isEmpty) startLine + 1 else document.getLineNumber(textRange.endOffset - 1) + 1
+        val nextSetBit = changedLines.nextSetBit(startLine)
+        return nextSetBit != -1 && nextSetBit < endLine
+    }
+
+    /**
+     * Taken from [VcsFacadeImpl.getRevisionedContentFrom]
+     */
+    private fun getRevisionedContentFrom(change: Change): String? {
+        val revision = change.beforeRevision ?: return null
+        return try {
+            revision.content
+        } catch (e: VcsException) {
+            log.warn("Can't get content for: " + change.virtualFile, e)
+            null
+        }
+    }
+
+    /**
+     * Taken from [VcsFacadeImpl.getRanges]
+     */
+    private fun getRanges(
+        document: Document,
+        contentFromVcs: CharSequence
+    ): List<Range> {
+        return createRanges(
+            document.immutableCharSequence,
+            StringUtilRt.convertLineSeparators(contentFromVcs, "\n")
+        )
     }
 }
