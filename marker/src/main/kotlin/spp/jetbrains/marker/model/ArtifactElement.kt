2198eaa1a230a1504cbd82445d18764224353adb
Brandon Fergerson <bfergerson@apache.org>
2022-12-12 20:56:19 +0400

    chore: impl equals/hashCode

--- marker/src/main/kotlin/spp/jetbrains/marker/model/ArtifactElement.kt
+++ marker/src/main/kotlin/spp/jetbrains/marker/model/ArtifactElement.kt
@@ -22,8 +22,21 @@ import spp.jetbrains.marker.service.getCalls
 import spp.jetbrains.marker.service.toArtifact
 
 open class ArtifactElement(private val psiElement: PsiElement) : PsiElement by psiElement {
+
     fun isControlStructure(): Boolean = this is ControlStructureArtifact
     fun isCall(): Boolean = this is CallArtifact
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as ArtifactElement
+        if (psiElement != other.psiElement) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return psiElement.hashCode()
+    }
 }
 
 // Extensions
