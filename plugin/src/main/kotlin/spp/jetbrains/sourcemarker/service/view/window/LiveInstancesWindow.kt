fa0cd8ab419d17025cdb54d8a75f0eb58616c3fb
Brandon Fergerson <bfergerson@apache.org>
2022-12-30 17:29:21 +0400

    style: detekt

--- plugin/src/main/kotlin/spp/jetbrains/sourcemarker/service/view/window/LiveInstancesWindow.kt
+++ plugin/src/main/kotlin/spp/jetbrains/sourcemarker/service/view/window/LiveInstancesWindow.kt
@@ -1,3 +1,19 @@
+/*
+ * Source++, the continuous feedback platform for developers.
+ * Copyright (C) 2022 CodeBrig, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package spp.jetbrains.sourcemarker.service.view.window
 
 import com.intellij.openapi.application.ApplicationManager
@@ -58,10 +74,10 @@ class LiveInstancesWindow(project: Project, service: Service) : LiveViewChartWin
         val vertx = UserData.vertx(project)
 //        ServiceBridge.currentServiceConsumer(vertx).handler {
 //            val service = it.body()
-            vertx.safeLaunch {
-                UserData.liveManagementService(project)?.getInstances(service.id)?.await()?.forEach {
-                    model.addRow(it)
-                }
+        vertx.safeLaunch {
+            UserData.liveManagementService(project)?.getInstances(service.id)?.await()?.forEach {
+                model.addRow(it)
+            }
 //            }
         }
     }
