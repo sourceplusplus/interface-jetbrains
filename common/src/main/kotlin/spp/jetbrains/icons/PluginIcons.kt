d7fc5e859a75dfec81b97af38e28234d2de39f2a
Brandon Fergerson <bfergerson@apache.org>
2023-01-01 17:15:13 +0400

    style: update header

--- common/src/main/kotlin/spp/jetbrains/icons/PluginIcons.kt
+++ common/src/main/kotlin/spp/jetbrains/icons/PluginIcons.kt
@@ -1,6 +1,6 @@
 /*
  * Source++, the continuous feedback platform for developers.
- * Copyright (C) 2022 CodeBrig, Inc.
+ * Copyright (C) 2022-2023 CodeBrig, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
