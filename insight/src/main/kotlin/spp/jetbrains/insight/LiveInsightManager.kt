880f841c4807a5d8bcc8a81c8a47264b14ab8991
Brandon Fergerson <bfergerson@apache.org>
2023-03-24 17:11:52 +0400

    style: detekt

--- insight/src/main/kotlin/spp/jetbrains/insight/LiveInsightManager.kt
+++ insight/src/main/kotlin/spp/jetbrains/insight/LiveInsightManager.kt
@@ -17,16 +17,12 @@
 package spp.jetbrains.insight
 
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.diagnostic.logger
-import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import spp.jetbrains.insight.contributor.FunctionDurationContributor
-import spp.jetbrains.marker.source.mark.api.ClassSourceMark
 import spp.jetbrains.marker.source.mark.api.event.SourceMarkEvent
 import spp.jetbrains.marker.source.mark.api.event.SourceMarkEventCode
 import spp.jetbrains.marker.source.mark.api.event.SourceMarkEventListener
-import spp.jetbrains.safeLaunch
 
 /**
  * When remote insights are unavailable, we fall back on local insights. Local insights require the
@@ -35,60 +31,17 @@ import spp.jetbrains.safeLaunch
  * insight instrumentation to perform more precise calculations.
  */
 class LiveInsightManager(
-    private val project: Project,
     private val remoteInsightsAvailable: Boolean
 ) : CoroutineVerticle(), SourceMarkEventListener, Disposable {
 
-    private val log = logger<LiveInsightManager>()
-
-    //    private lateinit var insightService: LiveInsightService
-//    private lateinit var workspace: InsightWorkspace
     private val contributors = listOf(
         FunctionDurationContributor(remoteInsightsAvailable)
     )
 
     override suspend fun start() {
         if (remoteInsightsAvailable) {
-            startRemoteInsights()
+            //startRemoteInsights()
         }
-    }
-
-    private suspend fun startRemoteInsights() {
-//        val analyzer = SourceAnalyzer(object : InsightStorage {
-//            override suspend fun <T> getInsightValue(type: InsightType, vararg args: PsiElement): T? {
-//                if (type == InsightType.FUNCTION_DURATION) {
-//                    val methodCall = args[0].reference
-//                    if (methodCall is PsiMethodCallExpression) {
-//                        val method = (args[0] as PsiMethodCallExpression).resolveMethod()
-//                        if (method != null && method.nameIdentifier != null) {
-////                            val resolvedMark = method.nameIdentifier!!.getUserData(SourceKey.GuideMark)
-////                            val duration = TODO() //resolvedMark?.getMethodDurationInsight()
-////                            if (duration != null) {
-////                                return duration as T
-////                            } else {
-////                                //query insight service
-////                                TODO()
-////                            }
-//                        }
-//                    }
-//                }
-//                //                val insights = insightService.getArtifactInsights(
-//                //                    workspace.id,
-//                //                    mark.artifactQualifiedName,
-//                //                    JsonArray().add(type.name)
-//                //                ).await()
-//                //                println(insights)
-//                return 200L as T
-//            }
-//        })
-//
-//        insightService = UserData.liveInsightService(project)!!
-//        if (insightService.getWorkspaces().await().isEmpty()) {
-//            workspace = insightService.createWorkspace().await()
-//        } else {
-//            workspace = insightService.getWorkspaces().await().first()
-//        }
-//        log.debug("Created workspace: ${workspace.id}")
     }
 
     override fun handleEvent(event: SourceMarkEvent) {
@@ -96,48 +49,13 @@ class LiveInsightManager(
             SourceMarkEventCode.MARK_ADDED -> {
                 if (event.sourceMark.isClassMark) {
                     if (remoteInsightsAvailable) {
-                        vertx.safeLaunch { downloadInsights(event.sourceMark as ClassSourceMark) }
+                        //vertx.safeLaunch { downloadInsights() }
                     }
                 }
             }
         }
 
         contributors.forEach { it.handleEvent(event) }
-    }
-
-    private suspend fun downloadInsights(classMark: ClassSourceMark) {
-//        //get head commit
-//        val change = ChangeListManager.getInstance(classMark.project)
-//            .getChange(classMark.sourceFileMarker.psiFile.virtualFile)
-//        println(change)
-//
-//        //upload source code
-//        insightService.uploadSourceCode(
-//            workspace.id,
-//            JsonObject()
-//                .put("file_path", classMark.sourceFileMarker.psiFile.virtualFile.path)
-//                .put("file_content", Buffer.buffer(classMark.sourceFileMarker.psiFile.text))
-//        ).await()
-//
-//        //trigger scan
-//        insightService.scanWorkspace(workspace.id).await()
-//
-//        //download insights
-//        classMark.sourceFileMarker.getGuideMarks().filter { it.isMethodMark }.forEach {
-//            val insights = insightService.getArtifactInsights(
-//                workspace.id,
-//                it.artifactQualifiedName,
-//                JsonArray().add(InsightType.FUNCTION_DURATION.name)
-//            ).await()
-//            it.putUserData(
-//                SourceMarkerKeys.FUNCTION_DURATION,
-//                InsightValue.of(
-//                    InsightType.FUNCTION_DURATION,
-//                    insights.getString(InsightType.FUNCTION_DURATION.name).toLong()
-//                )
-//            )
-//            println(insights)
-//        }
     }
 
     override suspend fun stop() = Disposer.dispose(this)
